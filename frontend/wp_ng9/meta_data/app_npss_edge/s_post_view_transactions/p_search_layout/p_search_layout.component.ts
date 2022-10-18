073d82484ba41ba212f1693c149d9cb46362f082
administrator <administrator@gsstvl.com>
2022-10-18 04:51:41 +0000

    5949 - changes in dashboard screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_post_view_transactions/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_post_view_transactions/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26508 
+Build ID        : 26502 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-18 2:50 AM 
+Modified Date   : 2022-Oct-18 4:51 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_post_view_transactions

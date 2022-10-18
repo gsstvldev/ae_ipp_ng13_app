02ca456e3af3a31e193fd9cd77d428c02f2c5e7e
administrator <administrator@gsstvl.com>
2022-10-18 06:23:25 +0000

    5954 - DT code added

--- frontend/wp_ng9/meta_data/app_npss_edge/s_hst_view_transactions/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_hst_view_transactions/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26515 
+Build ID        : 26520 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-18 5:21 AM 
+Modified Date   : 2022-Oct-18 6:23 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_hst_view_transactions
--- frontend/wp_ng9/meta_data/app_npss_edge/s_reversal_process/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_reversal_process/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26518 
+Build ID        : 26520 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-18 5:43 AM 
+Modified Date   : 2022-Oct-18 6:23 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_reversal_process

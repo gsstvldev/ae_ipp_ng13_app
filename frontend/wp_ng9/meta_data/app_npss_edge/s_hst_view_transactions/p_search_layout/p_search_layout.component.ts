15d1f1cf54a4121990307eb54e13d24b730ad1cf
administrator <administrator@gsstvl.com>
2022-10-18 02:51:00 +0000

    5946 - npsst id update

--- frontend/wp_ng9/meta_data/app_npss_edge/s_hst_view_transactions/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_hst_view_transactions/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26506 
+Build ID        : 26508 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-18 1:37 AM 
+Modified Date   : 2022-Oct-18 2:50 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_hst_view_transactions

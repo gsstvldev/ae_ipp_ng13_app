ad7ea5de349010b73610ef02ec78f9a50b6c5355
administrator <administrator@gsstvl.com>
2022-10-21 05:26:53 +0000

    6004 - Action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_transaction_details/f_refund_ui/f_refund_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_transaction_details/f_refund_ui/f_refund_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26600 
+Build ID        : 26609 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-21 5:5 AM 
+Modified Date   : 2022-Oct-21 5:26 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_transaction_details

36234e19989abb1b191ab0fc880463d1aa2179f4
administrator <administrator@gsstvl.com>
2022-10-21 05:06:43 +0000

    6003 - date format change

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/f_enable_refund_ui/f_enable_refund_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/f_enable_refund_ui/f_enable_refund_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26602 
+Build ID        : 26600 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-21 4:29 AM 
+Modified Date   : 2022-Oct-21 5:5 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_transactions

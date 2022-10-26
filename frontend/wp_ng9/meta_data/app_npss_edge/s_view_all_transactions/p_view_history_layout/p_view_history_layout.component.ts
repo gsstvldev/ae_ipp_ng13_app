f269edbc110e4e0e77717234ed2d524dc865247e
administrator <administrator@gsstvl.com>
2022-10-26 11:09:14 +0000

    6029 - Add column

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/p_view_history_layout/p_view_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/p_view_history_layout/p_view_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26619 
+Build ID        : 26658 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-21 14:27 PM 
+Modified Date   : 2022-Oct-26 11:4 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_transactions

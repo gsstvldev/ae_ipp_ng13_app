002c7588185d420275f729b8e7e0f073bef31c40
administrator <administrator@gsstvl.com>
2022-10-26 14:47:07 +0000

    6032 - MI level changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/s_view_all_transactions.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/s_view_all_transactions.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26658 
+Build ID        : 26661 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-26 11:4 AM 
+Modified Date   : 2022-Oct-26 14:45 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_transactions

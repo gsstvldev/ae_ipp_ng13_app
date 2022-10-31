3eb8ffce1db99b4458a6506e5cb49cc9ddc13365
administrator <administrator@gsstvl.com>
2022-10-31 09:16:34 +0000

    6080 - Merchant changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26735 
+Build ID        : 26738 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-31 7:24 AM 
+Modified Date   : 2022-Oct-31 9:15 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_merchant_details

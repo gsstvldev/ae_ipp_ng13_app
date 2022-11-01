4e9537c53daf2a3fd2b17e14155cbe5b51ad7ec8
administrator <administrator@gsstvl.com>
2022-11-01 05:13:17 +0000

    6093 - Delete CCD and query

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_navigation_ui/f_navigation_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_navigation_ui/f_navigation_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26750 
+Build ID        : 26754 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-31 13:20 PM 
+Modified Date   : 2022-Nov-01 4:54 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_investigation
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/s_view_all_transactions.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_transactions/s_view_all_transactions.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26751 
+Build ID        : 26754 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-31 14:30 PM 
+Modified Date   : 2022-Nov-01 4:54 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_transactions

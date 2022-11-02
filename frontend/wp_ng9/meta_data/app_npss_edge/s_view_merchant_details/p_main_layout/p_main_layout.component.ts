9377f18134279928c28ba90a48041df0497b68bf
administrator <administrator@gsstvl.com>
2022-11-02 04:41:41 +0000

    6101 - Merchant development corrections

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26738 
+Build ID        : 26767 
 Modified By     : Admin 
-Modified Date   : 2022-Oct-31 9:15 AM 
+Modified Date   : 2022-Nov-02 4:41 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_merchant_details

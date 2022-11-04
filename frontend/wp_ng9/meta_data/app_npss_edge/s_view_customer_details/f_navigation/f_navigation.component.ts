eb426ce5111a6ab64b1283ba41a0c2d1c1013fc9
administrator <administrator@gsstvl.com>
2022-11-04 07:20:50 +0000

    6123 - Menu item code concept

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_customer_details/f_navigation/f_navigation.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_customer_details/f_navigation/f_navigation.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26809 
+Build ID        : 26811 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-04 6:59 AM 
+Modified Date   : 2022-Nov-04 7:20 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_customer_details

a5a27f0725b0b91b28578c66b4442326ab3ac406
administrator <administrator@gsstvl.com>
2022-11-04 09:19:35 +0000

    6125 - filter changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_bct_transaction_details/p_vertical_layout/p_vertical_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_transaction_details/p_vertical_layout/p_vertical_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26814 
+Build ID        : 26816 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-04 7:52 AM 
+Modified Date   : 2022-Nov-04 9:19 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_bct_transaction_details

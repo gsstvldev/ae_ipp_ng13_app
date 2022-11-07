7b9089afa28ac138a37977f0de6a71c59837c986
administrator <administrator@gsstvl.com>
2022-11-07 10:45:08 +0000

    6145 - Add columns in merchant and customer related tables

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_customer_details/s_view_customer_details.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_customer_details/s_view_customer_details.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26827 
+Build ID        : 26843 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-04 14:36 PM 
+Modified Date   : 2022-Nov-07 10:43 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_customer_details

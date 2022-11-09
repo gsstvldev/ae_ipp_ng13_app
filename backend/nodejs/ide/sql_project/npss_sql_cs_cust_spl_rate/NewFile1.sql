2625f9daa52d931bde02f66cb6b4d8b8fd7aa182
administrator <administrator@gsstvl.com>
2022-11-09 07:39:58 +0000

    6153 - ide commits

--- backend/nodejs/ide/sql_project/npss_sql_cs_cust_spl_rate/NewFile1.sql
+++ backend/nodejs/ide/sql_project/npss_sql_cs_cust_spl_rate/NewFile1.sql
@@ -1,17 +1,14 @@
 CREATE TABLE IF NOT EXISTS CORE_NC_CUST_SPL_RATE
 (
-UNIT_ID character varying(10) COLLATE pg_catalog."default" not null,
 CIF_NUMBER character varying(10) COLLATE pg_catalog."default" not null,
-CCY_1 character varying(3) COLLATE pg_catalog."default" not null,
-CCY_2 character varying(3) COLLATE pg_catalog."default" not null,
-START_DATE timestamp without time zone not null,
-BUY_RATE numeric(16,8) not null,
-SELL_RATE numeric(16,8) not null,
-BUY_SPREAD numeric(16,8),
-SELL_SPREAD numeric(16,8),
-END_DATE timestamp without time zone,
-STATUS character(1) COLLATE pg_catalog."default" not null,
-ENTRY_DATE timestamp without time zone not null
+BUY_RATE decimal(16,8),
+SELL_RATE decimal(16,8),
+CURRENCY_CODE character varying(3) COLLATE pg_catalog."default" not null,
+BUY_MARGIN decimal(16,8),
+SELL_MARGIN decimal(16,8),
+TRAN_STAGE character varying(10) COLLATE pg_catalog."default",
+STATUS character(1) COLLATE pg_catalog."default",
+ENTRY_DATE timestamp without time zone
 )
 TABLESPACE pg_default;
 @SPL@

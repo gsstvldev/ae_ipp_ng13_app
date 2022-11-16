bf1ae93bd43d8ab84b2bafbabfc7d72d2fa23140
administrator <administrator@gsstvl.com>
2022-11-16 08:43:08 +0000

    6182 - DEV CHANGES AND update

--- frontend/wp_ng9/meta_data/app_npss_edge/s_bct_review_transactions/s_bct_review_transactions.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_review_transactions/s_bct_review_transactions.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26871 
+Build ID        : 26900 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-14 14:39 PM 
+Modified Date   : 2022-Nov-16 8:42 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_bct_review_transactions
--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_suspicious/f_npss_investigation_srch/f_npss_investigation_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_suspicious/f_npss_investigation_srch/f_npss_investigation_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26874 
+Build ID        : 26900 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-15 7:16 AM 
+Modified Date   : 2022-Nov-16 8:42 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_posting_suspicious

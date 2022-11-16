5df6f3bbc9a553c31938f46bab89af846e247f09
administrator <administrator@gsstvl.com>
2022-11-16 11:37:01 +0000

    6185 - Changes in Query

--- frontend/wp_ng9/meta_data/app_npss_edge/s_iban_transaction_details/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_iban_transaction_details/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26823 
+Build ID        : 26908 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-16 10:55 AM 
+Modified Date   : 2022-Nov-16 11:36 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_iban_transaction_details

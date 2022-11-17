4fa172495f4328aabae65f11ceb33982b473ea7f
administrator <administrator@gsstvl.com>
2022-11-17 12:57:15 +0000

    6195 - IDE Build

--- backend/nodejs/ide/api_service/npss_cs_bct_ip_investigation_change_state/index.js
+++ backend/nodejs/ide/api_service/npss_cs_bct_ip_investigation_change_state/index.js
@@ -6,7 +6,249 @@ var $REFPATH = Path.join(__dirname, '../../torus-references/');
 var app = express.Router();
 
 app.post('/', function(appRequest, appResponse, next) {
- console.log('success');
+
+    
+
+
+
+
+try {
+    /*  Created By : Daseen
+    Created Date :17/11/2022
+    Modified By : 
+    Modified Date : 
+    Reason for : 
+    */
+    var serviceName = 'NPSS BCT IP Invenstigation Change State';
+    var reqInstanceHelper = require($REFPATH + 'common/InstanceHelper'); ///  Response,error,info msg printing        
+    var reqTranDBInstance = require($REFPATH + "instance/TranDBInstance.js"); /// postgres & oracle DB pointing        
+    var reqLogInfo = require($REFPATH + 'log/trace/LogInfo'); /// Log information Detail 
+    var reqAuditLog = require($REFPATH + 'log/audit/AuditLog');
+    var reqDateFormatter = require($REFPATH + 'common/dateconverter/DateFormatter');
+    var params = appRequest.body.PARAMS; //  Client input fromm Server
+    var headers = appRequest.headers; // header details 
+    var objSessionLogInfo = null; // set value is null
+    var mTranConn = "";
+    var addquery = "";
+    var final_status,final_process_status,eligible_status,eligible_process_status,selectQry;
+    var id="";
+
+    var objresponse = {
+        'status': 'FAILURE',
+        'data': '',
+        'msg': ''
+    }; // Response to Client
+    // Assign function for loginformation and session info
+
+    reqLogInfo.AssignLogInfoDetail(appRequest, function (objLogInfo, objSessionInformation) {
+        try {
+            objSessionLogInfo = objLogInfo; // Assing log information
+            // Log Viewer Setup
+            objSessionLogInfo.HANDLER_CODE = 'NPSS BCT IP Invenstigation Change State';
+            objSessionLogInfo.ACTION = 'ACTION';
+            objSessionLogInfo.PROCESS = 'NPSS BCT IP Invenstigation Change State';
+            // Get DB Connection                                                                                                                                      
+            reqTranDBInstance.GetTranDBConn(headers, true, function (pSession) {
+                mTranConn = pSession; //  assign connection     
+                reqAuditLog.GetProcessToken(pSession, objLogInfo, function prct(error, prct_id) {
+                    try {
+
+
+                        var finalstatusqry = `select eligible_status,eligible_process_status, success_process_status,success_status from core_nc_workflow_setup where rule_code='${params.api_code}'`;
+
+                        ExecuteQuery1(finalstatusqry, function (arrResult) {
+                            if (arrResult.length) {
+                                final_status = arrResult[0].success_status;
+                             final_process_status = arrResult[0].success_process_status;
+                                eligible_status = arrResult[0].eligible_status;
+                                eligible_process_status = arrResult[0].eligible_process_status;
+                             selectQry = `select * from npss_transactions where status='${eligible_status}' and process_status='${eligible_process_status}' `;
+
+
+                                fn_MappingSearchParamQry(function callbackMappingSearchParamQry(pError, addquery) {
+                                    if (pError) {
+                                        sendResponse(pError)
+                                    } else {
+                                        var final_qry = selectQry + addquery;
+                                        ExecuteQuery1(final_qry, function (arrfinalResult) {
+                                            if (arrfinalResult.length > 0) {
+                                                for (var i in arrfinalResult) {
+                                                    id += "," + arrfinalResult[i].npsst_id;
+                                                }
+                                                id = id.slice(1);
+                                                var updateQry = `update npss_transactions set status='${final_status}',process_status='${final_process_status}' where npsst_id in(${id})`
+                                                ExecuteQuery(updateQry, function (arrfinalResult) {
+                                                    if (arrfinalResult == "SUCCESS") {
+                                                        sendResponse(null, arrfinalResult)
+                                                    }
+                                                    else {
+                                                        reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, "IDE_SERVICE_CORE_001", "Data not received from service.please check the log", result);
+                                                        sendResponse('Error in transaction update', result);
+                                                    }
+                                                })
+                                            }
+                                            else {
+                                                console.log("No Data found in Transaction");
+                                                objresponse.status = "FAILURE"
+                                                sendResponse(objresponse, null)
+                                            }
+                                        })
+
+
+                                    }
+                                })
+                            } else {
+                                console.log("No Data found in Rule Code");
+                                objresponse.status = "FAILURE"
+                                sendResponse(objresponse, null)
+                            }
+                        })
+
+                        //search param qry formation
+                        function fn_MappingSearchParamQry(callbackMappingSearchParamQry) {
+                            try {
+                                var addquery = "";
+                                var conditionObj = {};
+                                if (params.s_cdtr_iban) {
+                                    conditionObj["CDTR_IBAN"] = "CDTR_IBAN='" + params.s_cdtr_iban + "'";
+                                }
+                                if (params.s_uetr) {
+                                    conditionObj["UETR"] = "UETR='" + params.s_uetr + "'";
+                                }
+
+                                if (params.s_paymentmethod) {
+                                    conditionObj["paymentmethod"] = "MEM_PAYMENT_METHOD ='" + params.s_paymentmethod + "'";
+                                }
+                                if (params.s_valuedatevalue) {
+                                    conditionObj["valuedate"] = reqDateFormatter.GetSearchCriteriaForBusinessColumn(headers, objSessionLogInfo, 'VALUE_DATE', params.s_valuedatevalue, params.s_valuedatetovalue, params.s_valuedateop);
+                                }
+                                if (params.s_tran_ref_id) {
+                                    conditionObj["tran_ref_id"] = "TRAN_REF_ID ='" + params.s_tran_ref_id + "'";
+                                }
+
+                               
+                                if (params.s_tranamountvalue != null && params.s_tranamountvalue != undefined && params.s_tranamountvalue != "") {
+                                    if ((params.s_tranamountop == ">=") || (params.s_tranamountop == "Greater Than Or Equal")) {
+                                        conditionObj["tranamount"] = "INTRBK_STTLM_AMNT >='" + params.s_tranamountvalue + "'";
+                                    } else if ((params.s_tranamountop == "<=") || (params.s_tranamountop == "Less Than Or Equal")) {
+                                        conditionObj["tranamount"] = "INTRBK_STTLM_AMNT <='" + params.s_tranamountvalue + "'";
+                                    } else if (params.s_tranamountop == "") {
+                                        conditionObj["tranamount"] = "INTRBK_STTLM_AMNT ='" + params.s_tranamountvalue + "'";
+                                    }
+                                    else {
+                                        if (params.s_tranamounttovalue) {
+                                            conditionObj["tranamount"] = "INTRBK_STTLM_AMNT >='" + params.s_tranamountvalue + "' and INTRBK_STTLM_AMNT <='" + params.s_tranamounttovalue + "'";
+                                        }
+                                    }
+                                }
+                                if (params.s_e2eref) {
+                                    conditionObj["e2eref"] = "PAYMENT_ENDTOEND_ID='" + params.s_e2eref + "'";
+                                }
+                                if (params.s_cdtr_acct_no) {
+                                    conditionObj["cdtr_acct_no"] = "CDTR_ACCT_NO ='" + params.s_cdtr_acct_no + "'";
+                                }
+                                if (params.s_dbtr_acct_no) {
+                                    conditionObj["dbtr_acct_no"] = "DBTR_ACCT_NO ='" + params.s_dbtr_acct_no + "'";
+                                }
+
+                                if (params.s_createddatefromvalue) {
+                                    conditionObj["createdate"] = reqDateFormatter.GetSearchCriteriaForBusinessColumn(headers, objSessionLogInfo, 'created_date', params.s_createddatefromvalue, params.s_createddatetovalue, params.s_createddateop);
+                                }
+
+                                
+                                for (var i in conditionObj) {
+                                    addquery += "and " + conditionObj[i];
+                                }
+                                if(addquery==''){
+                                    addquery = addquery.slice(3);
+                                   
+                                }
+                               
+
+                                callbackMappingSearchParamQry(null, addquery);
+                            } catch (error) {
+                                callbackMappingSearchParamQry(error);
+                            }
+                        }
+
+
+
+                       
+                        //Execute Query Function
+                        function ExecuteQuery1(query, callback) {
+                            reqTranDBInstance.ExecuteSQLQuery(mTranConn, query, objSessionLogInfo, function (result, error) {
+                                try {
+                                    if (error) {
+                                        sendResponse(error, null);
+                                    } else {
+                                        if (result.rows.length > 0) {
+                                            callback(result.rows);
+                                        } else {
+                                            callback([]);
+                                        }
+                                    }
+                                } catch (error) {
+                                    sendResponse(error, null);
+                                }
+                            });
+                        }
+
+                          //Execute Query for common
+                          function ExecuteQuery(query, callback) {
+                            reqTranDBInstance.ExecuteSQLQuery(mTranConn, query, objSessionLogInfo, function (result, error) {
+                                try {
+                                    if (error) {
+                                        sendResponse(error)
+                                    } else {
+                                        callback("SUCCESS");
+
+                                    }
+                                } catch (error) {
+                                    sendResponse(error)
+                                }
+                            });
+                        }
+
+                        //Send Response Function Definition
+                        function sendResponse(error, response) {
+                            try {
+                                if (error) {
+                                    reqTranDBInstance.Commit(mTranConn, false, function callbackres(res) {
+                                        reqInstanceHelper.SendResponse(serviceName, appResponse, null, objSessionLogInfo, 'IDE_SERVICE_10005', '', error);
+                                    });
+                                } else {
+                                    reqTranDBInstance.Commit(mTranConn, true, function callbackres(res) {
+                                        reqInstanceHelper.SendResponse(serviceName, appResponse, response, objSessionLogInfo)
+                                    });
+                                }
+                            } catch (error) {
+                                reqInstanceHelper.SendResponse(serviceName, appResponse, null, objSessionLogInfo, 'IDE_SERVICE_10004', 'ERROR IN SEND RESPONSE FUNCTION : ', error);
+                            }
+                        }
+                    } catch (error) {
+                        sendResponse(error, null);
+                    }
+                })
+            })
+        } catch (error) {
+            sendResponse(error, null);
+        }
+    })
+
+
+
+
+
+}
+catch (error) {
+    sendResponse(error, null);
+}
+
+
+
+
+
+
 
 });
 
--- frontend/wp_ng9/custom_widget/npss_cs_bct_ip_investigation_change_state/npss_cs_bct_ip_investigation_change_state.service.ts
+++ frontend/wp_ng9/custom_widget/npss_cs_bct_ip_investigation_change_state/npss_cs_bct_ip_investigation_change_state.service.ts
@@ -1,19 +1,106 @@
 
 import { Injectable } from '@angular/core';
 //Uncomment below lines when it is required
-//import { Http, Response, Headers, RequestOptions, HttpModule } from '@angular/http';
+import { Http, Response, Headers, RequestOptions, HttpModule } from '@angular/http';
 //import { Observable } from 'rxjs/Observable';
 //import { HttpClient } from '@angular/common/http';
 //import 'rxjs/add/operator/map';
 //import 'rxjs/add/operator/catch';
-                        
+import { HttphelperService } from '../../scripts/fx/httphelper.service'; // for server call only use http services
+import { CoreService } from '../../scripts/fx/core.service'; //core services used for get screen values from memory variables
+import { AppHandlerService } from '../../scripts/fx/app.handler.service'; //Show for Dialog for Screen
+import { SessionService } from '../../scripts/fx/session.service'; // get Session information
+import { SCOPE } from '../../scripts/fx/session.enum'; // get Scope for Session level
+import { DialogService } from '../../scripts/fx/dialog.service'; // Dialog box appear 
 @Injectable()
 export class npss_cs_bct_ip_investigation_change_stateService {
-    constructor() { }
+    constructor(private http: Http,
+        private httpHelper: HttphelperService,
+        private coreHelper: CoreService,
+        private appHandler: AppHandlerService,
+        private sessionHelper: SessionService,
+        private dialogHelper: DialogService) { }
     //Default calling function
-    fn_npss_cs_bct_ip_investigation_change_state(source_id,destn_id,parent_source_id,event_code,event_params,screenInstance,internals,handler_code,event_data,data_source){
-    console.log('success')
+    fn_npss_cs_bct_ip_investigation_change_state(source_id, destn_id, parent_source_id, event_code, event_params, screenInstance, internals, handler_code, event_data, data_source) {
+        let Product_Code: any;
+        console.log('success')
+        let searchParamObj;
+        let search;
+
+        let App_Id: any = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "APP_ID")
+        if (App_Id == "3") {
+            Product_Code = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "S_CODE");
+        } else {
+            Product_Code = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "ROOT_SCODE");
+        }
+        let code: any;
+        if (Product_Code != null || Product_Code != undefined) {
+            code = Product_Code.split('_');
+        }
+        let Country_Code: any = code[1].slice(0, 2);
+        let client_code: any = code[1];
+        let system: any = code[0];
+
+        let ClientParams: any = {};
+        ///  Prepare input for Server call
+       
+        ClientParams.eligible_status = this.coreHelper.get_value_from_memory("MEMORY_VARIABLES", "MI_LEVEL_STATUS");
+        ClientParams.eligible_process_status = this.coreHelper.get_value_from_memory("MEMORY_VARIABLES", "MI_LEVEL_PROCESS_STATUS");
+        ClientParams.system = system
+        ClientParams.api_code = "BCT_IP_INVEST_INITIATE";
+
+
+        if (screenInstance.wftpa_description === "s_bct_investigation") {
+            search = screenInstance["search"].f_npss_investigation_srch.model;
+
+        } searchParamObj = {
+
+
+            "s_cdtr_iban": search.CDTR_IBAN,
+            "s_uetr": search.UETR,
+            "s_paymentmethod": search.memory67,
+            "s_tran_ref_id": search.TRAN_REF_ID.value,
+            "s_e2eref": search.PAYMENT_ENDTOEND_ID.value,
+            "s_cdtr_acct_no": search.CDTR_ACCT_NO,
+            "s_dbtr_acct_no": search.DBTR_ACCT_NO,
+            "s_tranamountop": search.INTRBK_STTLM_AMNT.operator,
+            "s_tranamountvalue": search.INTRBK_STTLM_AMNT.value,
+            "s_tranamounttovalue": search.INTRBK_STTLM_AMNT.tovalue,
+            
+            "s_createddatetovalue": search.memory63.tovalue,
+            "s_createddateop": search.memory63.operator,
+            "s_createddatefromvalue": search.memory63.value,
+            "s_valuedate": search.VALUE_DATE,
+            "s_valuedateop": search.VALUE_DATE.operator,
+            "s_valuedatetovalue": search.VALUE_DATE.tovalue,
+            "s_valuedatevalue": search.VALUE_DATE.value
+            
+
+        }
+        console.log("Search Params", searchParamObj);
+
+        Object.assign(ClientParams, searchParamObj);
+        console.log("Client Input Params with Search Params", ClientParams);
+
+        this.CallUrlWithData(ClientParams, screenInstance, internals);
+
     }
+
+CallUrlWithData(ClientParams, screenInstance, internals) {
+    this.httpHelper.HttpPost('/microsvc/npss_cs_bct_ip_investigation_change_state/', ClientParams)
+        .subscribe((res: any) => {
+            console.log("Bulk Change Status Server Response", res);
+            if (res.data == "SUCCESS") {
+                this.appHandler.callInternals(internals, screenInstance, "SUCCESS");
+            } else if (res.data == "FAILURE") {
+                this.appHandler.callInternals(internals, screenInstance, "FAILURE");
+            }  else {
+                this.dialogHelper.ShowInfoDialog(res.data);
+
+            }
+        });
+}
+
     //Custom validation logics
     //Uncomment below lines when validation is required
     //fn_customValidation(projName,screenInstance,message,callback){

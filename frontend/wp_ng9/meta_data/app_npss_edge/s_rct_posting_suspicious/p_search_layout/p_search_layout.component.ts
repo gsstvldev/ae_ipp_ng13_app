fa04e9ec4870b2cd257c6fec6db6f86e28858ad2
administrator <administrator@gsstvl.com>
2022-11-19 15:16:33 +0000

    6234 - Action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_suspicious/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_suspicious/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26990 
+Build ID        : 26993 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-19 12:37 PM 
+Modified Date   : 2022-Nov-19 15:15 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_posting_suspicious

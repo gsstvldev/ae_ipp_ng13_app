1e8dcd0a941f66e4bcfa131f3c0a6b353c42ebe0
administrator <administrator@gsstvl.com>
2022-11-22 08:13:32 +0000

    6250 - changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_npss_rct_post_plog_srch/f_npss_rct_post_plog_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_npss_rct_post_plog_srch/f_npss_rct_post_plog_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27010 
+Build ID        : 27011 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-22 7:39 AM 
+Modified Date   : 2022-Nov-22 8:12 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_investigation

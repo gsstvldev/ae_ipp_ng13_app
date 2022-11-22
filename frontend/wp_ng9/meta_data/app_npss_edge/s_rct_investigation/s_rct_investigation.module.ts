0f344fd89065ff93a75c3da1f34903dcd163632a
administrator <administrator@gsstvl.com>
2022-11-22 07:39:54 +0000

    6249 - RCT investigation correction

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/s_rct_investigation.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/s_rct_investigation.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27005 
+Build ID        : 27010 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-21 12:12 PM 
+Modified Date   : 2022-Nov-22 7:39 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_investigation
@@ -36,7 +36,7 @@ import { s_rct_investigationRoutingModule } from './s_rct_investigation.routing.
 import { p_main_layoutComponent } from './p_main_layout/p_main_layout.component'
 import { f_navigation_uiComponent } from './f_navigation_ui/f_navigation_ui.component'
 import { p_search_layoutComponent } from './p_search_layout/p_search_layout.component'
-import { f_npss_tran_srchComponent } from './f_npss_tran_srch/f_npss_tran_srch.component'
+import { f_npss_rct_post_plog_srchComponent } from './f_npss_rct_post_plog_srch/f_npss_rct_post_plog_srch.component'
 
 
 
@@ -53,7 +53,7 @@ import { f_npss_tran_srchComponent } from './f_npss_tran_srch/f_npss_tran_srch.c
     s_rct_investigationRoutingModule,
     torus_cs_show_hideModule,npss_cs_investigation_pac_028Module
   ],
-  declarations: [s_rct_investigationComponent,p_main_layoutComponent,f_navigation_uiComponent,p_search_layoutComponent,f_npss_tran_srchComponent,],
+  declarations: [s_rct_investigationComponent,p_main_layoutComponent,f_navigation_uiComponent,p_search_layoutComponent,f_npss_rct_post_plog_srchComponent,],
   providers:[AppHandlerService,ComponenthelperService]
 })
 

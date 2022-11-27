f41d315012220f7908ef6338f4286bbc0584c83c
administrator <administrator@gsstvl.com>
2022-11-27 06:36:29 +0000

    6302 - CURRENCYS

---
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_view_transactions_group/p_view_response_layout/p_view_response_layout.component.ts
@@ -0,0 +1,54 @@
+/* ---------------------------------------------------------------------------
+UI Framework    : Angular
+Version         : 5.0 
+Build ID        : 27094 
+Modified By     : Admin 
+Modified Date   : 2022-Nov-27 6:35 AM 
+Generated From  : TORUS Low Code Platform 
+Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
+Screen Name     : s_bct_view_transactions_group
+Profile Name    : View Response Layout--------------------------------------------------------------------------- */
+
+// Component Definition 
+import { Component, OnInit,Input } from '@angular/core';
+@Component({
+  selector: 'p_view_response_layout',
+  templateUrl: './p_view_response_layout.component.html',
+  styleUrls: ['./p_view_response_layout.component.css']
+})
+
+// Start of class 
+export class p_view_response_layoutComponent implements OnInit {
+  @Input() screen_instance: any;
+
+  // Variables 
+  	response_ui : any = {}
+	single_back_ui : any = {}
+
+
+  // Constructor 
+  constructor() { }
+  
+  // Page Init 
+  ngOnInit() {
+    	this.response_ui.desc = "Response UI"
+		this.response_ui.need_header = ""
+		this.single_back_ui.desc = "Single Back UI"
+		this.single_back_ui.need_header = ""
+	
+
+    this.screen_instance['p_view_response_layout'] = {};
+    this.screen_instance['p_view_response_layout']['checkHeader']=(component)=>{
+      if(this[component]!=undefined && this[component]["need_header"]!=undefined && (this[component]["need_header"]=="on"||this[component]["need_header"]==true)) {
+        return {hidden:false,desc:this[component]["desc"]};
+      } else {
+        return {hidden:true,desc:''};
+      }
+    } 
+  }
+  
+  isHidden(comp_id:string,view:string) {
+    return !(this.screen_instance[comp_id][view].show && this.screen_instance[comp_id].current_view==view);
+  }
+}
+// End of class 
--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_view_inward_payments/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_view_inward_payments/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 26996 
+Build ID        : 27094 
 Modified By     : Admin 
-Modified Date   : 2022-Nov-26 15:5 PM 
+Modified Date   : 2022-Nov-27 6:35 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_view_inward_payments

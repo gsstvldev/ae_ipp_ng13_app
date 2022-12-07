30e373c61d59ed841d7d629d4ce5871e3052907f
administrator <administrator@gsstvl.com>
2022-12-07 11:05:33 +0000

    6378 - Department code fiilter for group detail list

--- frontend/wp_ng9/meta_data/app_npss_edge/s_iban_view_transactions_group/p_view_main_req_and_res_layout/p_view_main_req_and_res_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_iban_view_transactions_group/p_view_main_req_and_res_layout/p_view_main_req_and_res_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27215 
+Build ID        : 27213 
 Modified By     : Admin 
-Modified Date   : 2022-Dec-07 7:46 AM 
+Modified Date   : 2022-Dec-07 11:1 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_iban_view_transactions_group

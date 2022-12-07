79e1b03ef2dabc29a5a9581770743391295aa967
administrator <administrator@gsstvl.com>
2022-12-07 07:47:36 +0000

    6377 - IDE COMMITSS

--- backend/nodejs/ide/sql_project/npss_sql_script_index/NewFile1.sql
+++ backend/nodejs/ide/sql_project/npss_sql_script_index/NewFile1.sql
@@ -4,6 +4,12 @@ create index if not exists ntpl_created_date on npss_trn_process_log (created_da
 drop index if exists nt_created_date ;
 create index if not exists nt_created_date on npss_transactions (created_date);
 @SPL@
+drop index if exists ntpl_status ;
+create index if not exists ntpl_status on npss_trn_process_log (status);
+@SPL@
+drop index if exists ntpl_queue ;
+create index if not exists ntpl_queue on npss_trn_process_log (process_status);
+@SPL@
 drop index if exists ncp_created_date ;
 create index if not exists ncp_created_date on npss_customer_proxy (created_date);
 @SPL@

7b62fda813927fd94975b3fdc763187e0397e59d
Tamilselvan K <tamilgsstvl@gmail.com>
2022-12-09 09:38:56 +0000

    Remove npss alter sql cbs accounts

--- backend/nodejs/ide/sql_project/npss_alter_sql_cs_cbs_accounts/NewFile1.sql
+++
@@ -1,6 +0,0 @@
-alter table CORE_NC_CBS_ACCOUNTS add COLUMN IF NOT EXISTS Virtual_Account_Name character varying(100);
-alter table CORE_NC_CBS_ACCOUNTS add COLUMN IF NOT EXISTS VA_Currency character varying(3);
-alter table CORE_NC_CBS_ACCOUNTS add COLUMN IF NOT EXISTS VA_Type character varying(35);
-alter table CORE_NC_CBS_ACCOUNTS add COLUMN IF NOT EXISTS VA_Status character varying(10);
-alter table CORE_NC_CBS_ACCOUNTS Drop Column IF EXISTS Account_type;
-@SPL@

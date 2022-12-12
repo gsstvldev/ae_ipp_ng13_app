186fdbce02c4c09e3a84ac4c1bf2eb6a3c479a30
administrator <administrator@gsstvl.com>
2022-12-12 12:53:48 +0000

    6433 - Module Name change and Index added

--- backend/nodejs/ide/sql_project/npss_sql_script_index/NewFile1.sql
+++ backend/nodejs/ide/sql_project/npss_sql_script_index/NewFile1.sql
@@ -16,3 +16,18 @@ create index if not exists ncp_created_date on npss_customer_proxy (created_date
 drop index if exists nmp_created_date ;
 create index if not exists nmp_created_date on npss_merchant_proxy (created_date);
 @SPL@
+drop index if exists ntpl_uetr ;
+create index if not exists ntpl_uetr on npss_trn_process_log (uetr);
+@SPL@
+drop index if exists nt_uetr ;
+create index if not exists nt_uetr on npss_transactions (uetr);
+@SPL@
+drop index if exists nt_status ;
+create index if not exists nt_status on npss_transactions (status);
+@SPL@
+drop index if exists nt_queue ;
+create index if not exists nt_queue on npss_transactions (process_status);
+@SPL@
+drop index if exists ntpl_processing_system ;
+create index if not exists ntpl_processing_system on npss_trn_process_log (processing_system);
+@SPL@

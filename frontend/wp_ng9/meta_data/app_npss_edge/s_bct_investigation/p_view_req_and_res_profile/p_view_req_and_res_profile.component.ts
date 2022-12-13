5e6ad5f9c2cee3c0ec8cf3e5676e3ea868c56e91
administrator <administrator@gsstvl.com>
2022-12-13 10:55:27 +0000

    6437 - column creations

--- frontend/wp_ng9/meta_data/app_npss_edge/s_bct_investigation/p_view_req_and_res_profile/p_view_req_and_res_profile.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_investigation/p_view_req_and_res_profile/p_view_req_and_res_profile.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27322 
+Build ID        : 27329 
 Modified By     : Admin 
-Modified Date   : 2022-Dec-13 6:14 AM 
+Modified Date   : 2022-Dec-13 10:54 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_bct_investigation

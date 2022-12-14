ed2a2378ee3cee5a6a1811f2058aade8c5539734
administrator <administrator@gsstvl.com>
2022-12-14 15:56:22 +0000

    6454 - Action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_customer_setup/p_action_layout/p_action_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_customer_setup/p_action_layout/p_action_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27354 
+Build ID        : 27357 
 Modified By     : Admin 
-Modified Date   : 2022-Dec-14 15:20 PM 
+Modified Date   : 2022-Dec-14 15:55 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_customer_setup

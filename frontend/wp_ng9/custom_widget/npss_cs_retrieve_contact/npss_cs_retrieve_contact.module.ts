272b5b01d735ac2b8fb6f92b59ba9f4fdd73180e
administrator <administrator@gsstvl.com>
2023-01-04 13:13:51 +0000

    6659 - fghghjjjhhhhjjh

--- frontend/wp_ng9/custom_widget/npss_cs_retrieve_contact/npss_cs_retrieve_contact.module.ts
+++ frontend/wp_ng9/custom_widget/npss_cs_retrieve_contact/npss_cs_retrieve_contact.module.ts
@@ -2,8 +2,10 @@
     import { NgModule } from '@angular/core';
     import { CommonModule } from '@angular/common';
     import { npss_cs_retrieve_contactComponent } from './npss_cs_retrieve_contact.component'; 
+    import { DialogModule } from 'primeng/dialog';
+    import { FormsModule } from '@angular/forms';
     @NgModule({
-        imports: [CommonModule],
+        imports: [CommonModule,DialogModule,FormsModule],
         exports:[npss_cs_retrieve_contactComponent],
         declarations: [npss_cs_retrieve_contactComponent],
         providers:[]

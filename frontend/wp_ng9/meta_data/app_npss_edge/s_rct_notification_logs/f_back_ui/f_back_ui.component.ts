1732d59458fd90594f6dd6cf3f884915d15e0955
administrator <administrator@gsstvl.com>
2023-01-06 08:16:52 +0000

    6673 - Report UI dev

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_notification_logs/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_notification_logs/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27720 
+Build ID        : 27721 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-06 7:11 AM 
+Modified Date   : 2023-Jan-06 8:16 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_notification_logs

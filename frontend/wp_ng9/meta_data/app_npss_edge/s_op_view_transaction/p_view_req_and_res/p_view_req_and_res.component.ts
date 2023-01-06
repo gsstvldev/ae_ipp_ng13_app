f9de392cb79f4ceab5c1cc0f6faf356ea00d6ebf
administrator <administrator@gsstvl.com>
2023-01-06 04:57:46 +0000

    6669 - notification log screen dev

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_view_transaction/p_view_req_and_res/p_view_req_and_res.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_view_transaction/p_view_req_and_res/p_view_req_and_res.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27713 
+Build ID        : 27715 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-05 15:49 PM 
+Modified Date   : 2023-Jan-06 4:55 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_view_transaction

aa9b53b26d31a641c71d9766c7b0b1df1460b075
administrator <administrator@gsstvl.com>
2023-01-06 08:33:34 +0000

    6674 - ffgfg

---
+++ frontend/wp_ng9/custom_widget/npss_cs_communication_send_outward_ipp_payment_ntf/npss_cs_communication_send_outward_ipp_payment_ntf.module.ts
@@ -0,0 +1,12 @@
+
+    import { NgModule } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+    import { npss_cs_communication_send_outward_ipp_payment_ntfComponent } from './npss_cs_communication_send_outward_ipp_payment_ntf.component'; 
+    @NgModule({
+        imports: [CommonModule],
+        exports:[npss_cs_communication_send_outward_ipp_payment_ntfComponent],
+        declarations: [npss_cs_communication_send_outward_ipp_payment_ntfComponent],
+        providers:[]
+    })
+    // Start of class 
+    export class npss_cs_communication_send_outward_ipp_payment_ntfModule { }

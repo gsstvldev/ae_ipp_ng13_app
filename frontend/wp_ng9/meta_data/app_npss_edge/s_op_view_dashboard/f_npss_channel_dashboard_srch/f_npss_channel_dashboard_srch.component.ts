3fed99d8a1af24e2962fc07ae9dbee5d60cbeec6
administrator <administrator@gsstvl.com>
2023-01-10 08:18:09 +0000

    6700 - Query changes for review screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_view_dashboard/f_npss_channel_dashboard_srch/f_npss_channel_dashboard_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_view_dashboard/f_npss_channel_dashboard_srch/f_npss_channel_dashboard_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27773 
+Build ID        : 27764 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-10 5:43 AM 
+Modified Date   : 2023-Jan-10 8:10 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_view_dashboard

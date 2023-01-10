26a9a12142462f4e14937bff2d2547f34c496b73
administrator <administrator@gsstvl.com>
2023-01-10 10:36:01 +0000

    6702 - changes for ide commits

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_remarks_ui/f_npss_remarks_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_remarks_ui/f_npss_remarks_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27776 
+Build ID        : 27777 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-10 9:44 AM 
+Modified Date   : 2023-Jan-10 10:35 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal

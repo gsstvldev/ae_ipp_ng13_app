cad2e818a2f5d7f7fbc30034b2749df2f22aceac
administrator <administrator@gsstvl.com>
2023-01-12 09:42:46 +0000

    6713 - BILD

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_messages/f_navigation_ui/f_navigation_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_messages/f_navigation_ui/f_navigation_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27798 
+Build ID        : 27771 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-12 8:28 AM 
+Modified Date   : 2023-Jan-12 9:42 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_ip_transaction_by_messages

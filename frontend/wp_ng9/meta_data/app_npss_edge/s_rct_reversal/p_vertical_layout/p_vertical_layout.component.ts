360f9bc20606f3431071385bcfaf6a54382d05ce
administrator <administrator@gsstvl.com>
2023-01-13 08:08:17 +0000

    6716 - Dashboard screen dev

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/p_vertical_layout/p_vertical_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/p_vertical_layout/p_vertical_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27802 
+Build ID        : 27814 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-12 13:8 PM 
+Modified Date   : 2023-Jan-13 8:5 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal

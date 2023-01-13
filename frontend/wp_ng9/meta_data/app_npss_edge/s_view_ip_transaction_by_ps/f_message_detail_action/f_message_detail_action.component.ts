8980265fdfb93ab68d8c9ce4442c3532b4d76bc5
administrator <administrator@gsstvl.com>
2023-01-13 15:40:35 +0000

    6726 - Action level changes queue

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_ps/f_message_detail_action/f_message_detail_action.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_ps/f_message_detail_action/f_message_detail_action.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27829 
+Build ID        : 27834 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-13 15:9 PM 
+Modified Date   : 2023-Jan-13 15:38 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_ip_transaction_by_ps

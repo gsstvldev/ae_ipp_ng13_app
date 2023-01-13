912228a6bc4c6048de3ba5f090f74f286714ce5f
administrator <administrator@gsstvl.com>
2023-01-13 11:49:56 +0000

    6721 - Role Changes for Dashboard screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_p2p_view_outward_payments/s_p2p_view_outward_payments.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_p2p_view_outward_payments/s_p2p_view_outward_payments.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27800 
+Build ID        : 27823 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-12 12:47 PM 
+Modified Date   : 2023-Jan-13 11:45 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_p2p_view_outward_payments
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_ps/p_view_history_layout/p_view_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_ip_transaction_by_ps/p_view_history_layout/p_view_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27812 
+Build ID        : 27823 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-13 8:5 AM 
+Modified Date   : 2023-Jan-13 11:45 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_ip_transaction_by_ps

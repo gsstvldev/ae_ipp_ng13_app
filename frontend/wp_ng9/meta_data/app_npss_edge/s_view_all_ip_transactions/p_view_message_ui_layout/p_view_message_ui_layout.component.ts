8372b886001bf43f5542b7e784fa8581574179a1
administrator <administrator@gsstvl.com>
2023-01-20 14:39:51 +0000

    6750 - Table deletion

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/p_view_message_ui_layout/p_view_message_ui_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/p_view_message_ui_layout/p_view_message_ui_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27849 
+Build ID        : 27885 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-20 11:6 AM 
+Modified Date   : 2023-Jan-20 14:39 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_ip_transactions

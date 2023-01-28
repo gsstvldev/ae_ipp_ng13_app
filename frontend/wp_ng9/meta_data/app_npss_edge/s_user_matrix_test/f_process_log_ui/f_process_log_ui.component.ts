5b9cfc79cd5e53d21ee899def8d1c95e01ba8ecc
administrator <administrator@gsstvl.com>
2023-01-28 10:27:15 +0000

    6772 - Testcase for filter setup3

--- frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/f_process_log_ui/f_process_log_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/f_process_log_ui/f_process_log_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27933 
+Build ID        : 27934 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-28 8:26 AM 
+Modified Date   : 2023-Jan-28 10:26 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_user_matrix_test

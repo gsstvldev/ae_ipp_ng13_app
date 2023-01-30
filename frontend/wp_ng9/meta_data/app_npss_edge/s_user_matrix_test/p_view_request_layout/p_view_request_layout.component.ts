8a3db98b279e7e5bd4718323a935a20a3378bcc1
administrator <administrator@gsstvl.com>
2023-01-30 07:03:51 +0000

    6776 - Department code filter setup

--- frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/p_view_request_layout/p_view_request_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/p_view_request_layout/p_view_request_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27937 
+Build ID        : 27941 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-30 6:14 AM 
+Modified Date   : 2023-Jan-30 7:3 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_user_matrix_test

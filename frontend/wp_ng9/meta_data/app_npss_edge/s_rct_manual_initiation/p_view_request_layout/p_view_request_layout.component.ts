25f6d466b240545303c0d7fac2eae15c39854ffc
administrator <administrator@gsstvl.com>
2023-01-31 10:12:03 +0000

    6786 - op payment dtls report search controls

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_view_request_layout/p_view_request_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_view_request_layout/p_view_request_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27959 
+Build ID        : 27961 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-31 6:6 AM 
+Modified Date   : 2023-Jan-31 10:10 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_initiation

02caba304e4d2b03e68ffdcaa0ceacca0ea983e9
administrator <administrator@gsstvl.com>
2023-02-02 05:35:37 +0000

    6799 - Adding a new field(PROCESS_GROUP) in transaction table

--- frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_user_matrix_test/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 27941 
+Build ID        : 27978 
 Modified By     : Admin 
-Modified Date   : 2023-Jan-30 7:3 AM 
+Modified Date   : 2023-Feb-02 5:34 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_user_matrix_test

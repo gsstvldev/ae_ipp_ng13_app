dfe1d276ca5aaf4732f2c7908bdbf9019b4d95f5
administrator <administrator@gsstvl.com>
2023-02-14 11:15:37 +0000

    6866 - CHANGES FOR UPDATE

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28093 
+Build ID        : 28111 
 Modified By     : Admin 
-Modified Date   : 2023-Feb-13 13:3 PM 
+Modified Date   : 2023-Feb-14 11:14 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_initiation

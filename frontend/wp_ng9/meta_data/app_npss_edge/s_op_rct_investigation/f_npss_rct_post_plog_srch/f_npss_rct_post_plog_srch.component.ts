dc1db05591d73b05e1922059e64b26a7a929e613
administrator <administrator@gsstvl.com>
2023-02-14 06:14:54 +0000

    6858 - invest screen dev

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_npss_rct_post_plog_srch/f_npss_rct_post_plog_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_npss_rct_post_plog_srch/f_npss_rct_post_plog_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28099 
+Build ID        : 28098 
 Modified By     : Admin 
-Modified Date   : 2023-Feb-14 4:43 AM 
+Modified Date   : 2023-Feb-14 6:13 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_rct_investigation

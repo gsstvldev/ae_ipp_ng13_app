1077daa3624b0b10e2bc04a466018cbc7b2e5d75
administrator <administrator@gsstvl.com>
2023-02-16 14:56:50 +0000

    6890 - CHANGES FOR Query

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28113 
+Build ID        : 28145 
 Modified By     : Admin 
-Modified Date   : 2023-Feb-15 15:0 PM 
+Modified Date   : 2023-Feb-16 14:55 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_rct_investigation
--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_returns/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_returns/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28135 
+Build ID        : 28145 
 Modified By     : Admin 
-Modified Date   : 2023-Feb-16 11:31 AM 
+Modified Date   : 2023-Feb-16 14:55 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_rejected_returns

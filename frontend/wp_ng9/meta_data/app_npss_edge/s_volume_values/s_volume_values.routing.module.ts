4bba2aad72d8738f0e509876aa0fdcf9e7c9ad00
administrator <administrator@gsstvl.com>
2023-02-25 15:30:48 +0000

    7003 - dfgssdsddf

--- frontend/wp_ng9/meta_data/app_npss_edge/s_volume_values/s_volume_values.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_volume_values/s_volume_values.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28341 
+Build ID        : 28344 
 Modified By     : Admin 
-Modified Date   : 2023-Feb-25 14:37 PM 
+Modified Date   : 2023-Feb-25 15:30 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_volume_values

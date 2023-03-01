4c40aed4ad859c8eba12f8fcf15f60081113923b
administrator <administrator@gsstvl.com>
2023-03-01 11:55:16 +0000

    7039 - REVERSAL SCREEN DEV

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/p_reversal_new__layout/p_reversal_new__layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/p_reversal_new__layout/p_reversal_new__layout.component.ts
@@ -7,23 +7,26 @@ Modified Date   : 2023-Mar-01 11:54 AM
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal
-Profile Name    : View Request layout--------------------------------------------------------------------------- */
+Profile Name    : Reversal New  Layout--------------------------------------------------------------------------- */
 
 // Component Definition 
 import { Component, OnInit,Input } from '@angular/core';
 @Component({
-  selector: 'p_view_request_layout',
-  templateUrl: './p_view_request_layout.component.html',
-  styleUrls: ['./p_view_request_layout.component.css']
+  selector: 'p_reversal_new__layout',
+  templateUrl: './p_reversal_new__layout.component.html',
+  styleUrls: ['./p_reversal_new__layout.component.css']
 })
 
 // Start of class 
-export class p_view_request_layoutComponent implements OnInit {
+export class p_reversal_new__layoutComponent implements OnInit {
   @Input() screen_instance: any;
 
   // Variables 
-  	request_ui : any = {}
-	single_back_ui : any = {}
+  	vertical_list : any = {}
+	reversal_ui : any = {}
+	cbs_accounts : any = {}
+	process_log_list : any = {}
+	reversal_action_ui : any = {}
 
 
   // Constructor 
@@ -31,14 +34,20 @@ export class p_view_request_layoutComponent implements OnInit {
   
   // Page Init 
   ngOnInit() {
-    	this.request_ui.desc = "Request UI"
-		this.request_ui.need_header = ""
-		this.single_back_ui.desc = "Single Back UI"
-		this.single_back_ui.need_header = ""
+    	this.vertical_list.desc = "Vertical List"
+		this.vertical_list.need_header = ""
+		this.reversal_ui.desc = "Reversal UI"
+		this.reversal_ui.need_header = ""
+		this.cbs_accounts.desc = "CBS Accounts"
+		this.cbs_accounts.need_header = true
+		this.process_log_list.desc = "Process Log List"
+		this.process_log_list.need_header = true
+		this.reversal_action_ui.desc = "Reversal Action UI"
+		this.reversal_action_ui.need_header = ""
 	
 
-    this.screen_instance['p_view_request_layout'] = {};
-    this.screen_instance['p_view_request_layout']['checkHeader']=(component)=>{
+    this.screen_instance['p_reversal_new__layout'] = {};
+    this.screen_instance['p_reversal_new__layout']['checkHeader']=(component)=>{
       if(this[component]!=undefined && this[component]["need_header"]!=undefined && (this[component]["need_header"]=="on"||this[component]["need_header"]==true)) {
         return {hidden:false,desc:this[component]["desc"]};
       } else {

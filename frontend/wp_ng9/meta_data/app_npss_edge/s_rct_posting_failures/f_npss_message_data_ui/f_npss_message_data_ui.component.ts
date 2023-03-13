607b7ebcb7fe9126003ab27106d25d6e2c49daa9
administrator <administrator@gsstvl.com>
2023-03-13 13:03:34 +0000

    7166 - Bug 1837

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_failures/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_failures/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28623 
+Build ID        : 28636 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-10 13:6 PM 
+Modified Date   : 2023-Mar-13 13:2 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_posting_failures

b6817c8bcb97034d74e67c93a23fe73f39337a22
administrator <administrator@gsstvl.com>
2023-03-16 12:24:21 +0000

    7213 - Filter changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28721 
+Build ID        : 28723 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-16 11:35 AM 
+Modified Date   : 2023-Mar-16 12:21 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

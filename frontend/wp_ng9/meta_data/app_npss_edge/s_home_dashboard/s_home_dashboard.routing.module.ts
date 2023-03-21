e5bf880413ae2ab162951143ff5f1c4de3ec04ec
administrator <administrator@gsstvl.com>
2023-03-21 14:31:39 +0000

    7277 - Home Dashboard

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/s_home_dashboard.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/s_home_dashboard.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28787 
+Build ID        : 28808 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-21 10:46 AM 
+Modified Date   : 2023-Mar-21 14:29 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

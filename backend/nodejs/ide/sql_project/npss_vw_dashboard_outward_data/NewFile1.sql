58e26dad63630f335cd2828fc7e31eedbfd8aed4
administrator <administrator@gsstvl.com>
2023-03-21 15:12:23 +0000

    7279 - YGHBHHBH

--- backend/nodejs/ide/sql_project/npss_vw_dashboard_outward_data/NewFile1.sql
+++ backend/nodejs/ide/sql_project/npss_vw_dashboard_outward_data/NewFile1.sql
@@ -3,7 +3,23 @@
 drop view if exists vw_dashboard_outward_data;
 @SPL@
 CREATE OR REPLACE VIEW ad_gss_tran.vw_dashboard_outward_data
-AS SELECT res.type,
+AS select V.sno,V.type,SUM (V.total) as total,
+                    SUM (V.ibmb) as ibmb,
+                    SUM (V.rib) as rib,
+                    SUM (V.rmb) as rmb,
+                    SUM (V.cib) as cib,
+                    SUM (V.cms) as cms,
+                    SUM (V.cmb) as cmb,
+                    SUM (V.manual) as manual,
+                    SUM (V.pending_screening) as pending_screening,
+                    SUM (V.pending_maker) as pending_maker,
+                    SUM (V.pending_checker) as pending_checker,
+                    SUM (V.send_to_cb) as send_to_cb,
+                    SUM (V.cback) as cback,
+                    SUM (V.cbnack) as cbnack,
+                    V.created_date,
+                    SUM (V.pending_t_1) as pending_t_1,
+                    V.DEPARTMENT_CODE from (SELECT res.type,
     COALESCE(sum(res.total), 0::numeric) AS total,
     COALESCE(sum(res.ibmb), 0::numeric) AS ibmb,
     COALESCE(sum(res.rib), 0::numeric) AS rib,
@@ -24,21 +40,27 @@ AS SELECT res.type,
                FROM npss_transactions nppst
                  LEFT JOIN npss_trn_process_log npl ON npl.uetr::text = nppst.uetr::text
               WHERE nppst.process_type::text = 'OP'::text AND (npl.process_name::text = ANY (ARRAY['Place Pacs008'::character varying::text])) AND nppst.process_status::text = 'RCTExceptionFailure'::text AND (nppst.status::text = ANY (ARRAY['OP_AC_REV_POSTING_FAILURE'::character varying::text, 'OP_AC_RET_POSTING_FAILURE'::character varying::text, 'OP_P2P_REV_POSTING_FAILURE'::character varying::text, 'OP_P2B_FUND_UNFR_FAILURE'::character varying::text, 'OP_P2B_REV_POSTING_FAILURE'::character varying::text, 'OP_AC_REV_POSTING_RETRY'::character varying::text, 'OP_AC_RET_POSTING_RETRY'::character varying::text, 'OP_P2P_REV_POSTING_RETRY'::character varying::text, 'OP_P2B_FUND_UNFR_RETRY'::character varying::text, 'OP_P2B_REV_POSTING_RETRY'::character varying::text])) 
+            and NPPST.department_code = RES.DEPARTMENT_CODE
               AND to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) < CURRENT_DATE) as Pen08t1)
             WHEN res.type = 'pacs.004'::text THEN ( select count(*) from (
           SELECT distinct  process_NAME,nppst.npsst_id 
                FROM npss_transactions nppst
                  LEFT JOIN npss_trn_process_log npl ON npl.uetr::text = nppst.uetr::text
-              WHERE nppst.process_type::text = 'OP'::text AND (npl.process_name::text = ANY (ARRAY['Receive Pacs004'::character varying::text])) AND nppst.process_status::text = 'RCTExceptionFailure'::text AND (nppst.status::text = ANY (ARRAY['OR_P2B_POSTING_FAILURE'::character varying::text, 'OR_P2P_POSTING_FAILURE'::character varying::text, 'OR_P2P_POSTING_RETRY'::character varying::text, 'OR_P2B_POSTING_RETRY'::character varying::text])) AND to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) < CURRENT_DATE) as Pen04t1)
+              WHERE nppst.process_type::text = 'OP'::text 
+              and NPPST.department_code = RES.DEPARTMENT_CODE
+              AND (npl.process_name::text = ANY (ARRAY['Receive Pacs004'::character varying::text])) AND nppst.process_status::text = 'RCTExceptionFailure'::text AND (nppst.status::text = ANY (ARRAY['OR_P2B_POSTING_FAILURE'::character varying::text, 'OR_P2P_POSTING_FAILURE'::character varying::text, 'OR_P2P_POSTING_RETRY'::character varying::text, 'OR_P2B_POSTING_RETRY'::character varying::text])) AND to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) < CURRENT_DATE) as Pen04t1)
             WHEN res.type = 'pacs.007'::text THEN ( select count(*) from (
           SELECT distinct  process_NAME,nppst.npsst_id 
                FROM npss_transactions nppst
                  LEFT JOIN npss_trn_process_log npl ON npl.uetr::text = nppst.uetr::text
-              WHERE nppst.process_type::text = 'OP'::text AND (npl.process_name::text = ANY (ARRAY['Place Pacs.007'::character varying::text])) AND (nppst.status::text = ANY (ARRAY['OP_REVERSAL_REQ_REPAIR'::character varying::text, 'OP_REVERSAL_REQ_INITIATED'::character varying::text])) AND to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) < CURRENT_DATE) as Pen07t1)
+              WHERE nppst.process_type::text = 'OP'::text 
+              and NPPST.department_code = RES.DEPARTMENT_CODE
+              AND (npl.process_name::text = ANY (ARRAY['Place Pacs.007'::character varying::text])) AND (nppst.status::text = ANY (ARRAY['OP_REVERSAL_REQ_REPAIR'::character varying::text, 'OP_REVERSAL_REQ_INITIATED'::character varying::text])) AND to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) < CURRENT_DATE) as Pen07t1)
             ELSE 0::bigint
         END AS pending_t_1,
     res.created_date,
-    res.sno
+    res.sno,
+    res.department_code
    FROM ( SELECT
                 CASE
                     WHEN npl.process_name::text = 'Place Pacs008'::text THEN 1
@@ -148,12 +170,16 @@ AS SELECT res.type,
                     WHEN npl.process_name::text = 'Place Pacs008'::text AND (nppst.status::text = 'OP_AC_REV_POSTING_SUCCESS'::text OR nppst.status::text = 'OP_AC_REV_POSTING_SUCCESS'::text OR nppst.process_status::text = 'OP_AC_STATUS_REJECTED'::text OR nppst.process_status::text = 'OP_P2P_REV_POSTING_SUCCESS'::text OR nppst.process_status::text = 'OP_P2B_STATUS_REJECTED'::text OR nppst.process_status::text = 'OP_P2B_REV_POSTING_SUCCESS'::text) THEN nppst.npsst_id
                     ELSE NULL::integer
                 END) AS cbnack,
-            to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date
+            to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date,
+            case when 
+            (nppst.department_code = '') or (nppst.department_code = NULL) then 'NA'
+            else department_code end department_code
            FROM npss_transactions nppst
              LEFT JOIN npss_trn_process_log npl ON npl.uetr::text = nppst.uetr::text
-          WHERE nppst.process_type::text = 'OP'::text AND (npl.process_name::text = ANY (ARRAY['Place Pacs008'::character varying::text, 'Place Pacs.007'::character varying::text, 'Receive Pacs004'::character varying::text, 'PACS.008'::character varying::text, 'PACS.007'::character varying::text]))
-          GROUP BY nppst.channel_id, npl.process_name, nppst.process_status, nppst.status, nppst.process_type, nppst.process_group, npl.status, npl.process_status, (to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text))
-        UNION ALL (
+          WHERE to_date(to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text), 'yyyy-mm-dd'::text) = CURRENT_DATE AND 
+          nppst.process_type::text = 'OP'::text AND (npl.process_name::text = ANY (ARRAY['Place Pacs008'::character varying::text, 'Place Pacs.007'::character varying::text, 'Receive Pacs004'::character varying::text, 'PACS.008'::character varying::text, 'PACS.007'::character varying::text]))
+          GROUP BY nppst.channel_id, npl.process_name, nppst.process_status, nppst.status, nppst.process_type, nppst.process_group, npl.status, npl.process_status,nppst.department_code, (to_char(nppst.created_date::date::timestamp with time zone, 'yyyy-mm-dd'::text))
+          UNION ALL (
                  SELECT 1 AS sno,
                     'pacs.008'::text AS type,
                     0 AS total,
@@ -170,7 +196,8 @@ AS SELECT res.type,
                     0 AS send_to_cb,
                     0 AS cback,
                     0 AS cbnack,
-                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date
+                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date,
+                    'NA' as DEPARTMENT_CODE
                 UNION
                  SELECT 3 AS sno,
                     'pacs.007'::text AS type,
@@ -188,7 +215,8 @@ AS SELECT res.type,
                     0 AS send_to_cb,
                     0 AS cback,
                     0 AS cbnack,
-                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date
+                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date,
+                    'NA' as DEPARTMENT_CODE
                 UNION
                  SELECT 2 AS sno,
                     'pacs.004'::text AS type,
@@ -206,7 +234,9 @@ AS SELECT res.type,
                     0 AS send_to_cb,
                     0 AS cback,
                     0 AS cbnack,
-                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date
+                    to_char(CURRENT_DATE::timestamp with time zone, 'yyyy-mm-dd'::text)::timestamp without time zone AS created_date,
+                    'NA' as DEPARTMENT_CODE
         )) res
-  GROUP BY res.type, res.created_date, res.sno
-  ORDER BY res.sno;
+  GROUP BY res.type, res.created_date, res.sno,res.department_code
+  ) as V 
+  group by V.sno,V.type,V.created_date,V.DEPARTMENT_CODE;
--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_process_log_ui/f_process_log_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_process_log_ui/f_process_log_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28818 
+Build ID        : 28819 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-21 15:8 PM 
+Modified Date   : 2023-Mar-21 15:11 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

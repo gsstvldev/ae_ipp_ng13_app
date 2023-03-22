b530283e667bbd705f3f078c6d9311c1dba466f9
administrator <administrator@gsstvl.com>
2023-03-22 21:42:32 +0000

    7297 - cgg

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_default_profile/p_default_profile.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_default_profile/p_default_profile.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28846 
+Build ID        : 28850 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-22 21:1 PM 
+Modified Date   : 2023-Mar-22 21:41 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard
--- frontend/wp_ng9/meta_data/app_npss_edge/s_master_dashboard/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_master_dashboard/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28849 
+Build ID        : 28850 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-22 21:9 PM 
+Modified Date   : 2023-Mar-22 21:41 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_master_dashboard

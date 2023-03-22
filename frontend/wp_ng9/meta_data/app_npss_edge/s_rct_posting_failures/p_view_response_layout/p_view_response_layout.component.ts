30c90258a82902cbcec433500aad188d98ad0f10
administrator <administrator@gsstvl.com>
2023-03-22 18:40:44 +0000

    7294 - qUERY CHANGES

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_failures/p_view_response_layout/p_view_response_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_posting_failures/p_view_response_layout/p_view_response_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28813 
+Build ID        : 28845 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-22 11:1 AM 
+Modified Date   : 2023-Mar-22 18:39 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_posting_failures

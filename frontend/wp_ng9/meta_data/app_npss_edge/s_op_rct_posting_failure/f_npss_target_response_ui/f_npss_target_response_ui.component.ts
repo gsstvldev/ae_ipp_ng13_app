8f75a3f92f37b3f011674c657ee221bada29e3cf
administrator <administrator@gsstvl.com>
2023-03-22 11:49:41 +0000

    7289 - NPSS Reject Payment CB Queue MQ

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_posting_failure/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_posting_failure/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28813 
+Build ID        : 28833 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-22 11:1 AM 
+Modified Date   : 2023-Mar-22 11:44 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_rct_posting_failure

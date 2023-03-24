a8a2a3923617340a380988d1b307364a68fd73bc
administrator <administrator@gsstvl.com>
2023-03-24 10:22:22 +0000

    7317 - Layout corrections

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_default_profile/p_default_profile.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_default_profile/p_default_profile.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28877 
+Build ID        : 28879 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-24 8:9 AM 
+Modified Date   : 2023-Mar-24 10:21 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

d63f36a67316c3e5d0a2dad96eab5dad8b051cb8
administrator <administrator@gsstvl.com>
2023-03-29 12:28:37 +0000

    7373 - Action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28949 
+Build ID        : 28952 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-29 7:7 AM 
+Modified Date   : 2023-Mar-29 12:26 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_verification
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/s_view_all_ip_transactions.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/s_view_all_ip_transactions.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28949 
+Build ID        : 28952 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-29 7:8 AM 
+Modified Date   : 2023-Mar-29 12:26 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_ip_transactions

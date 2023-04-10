4a3c939827730d10135b5447bcca0dd1acffef71
administrator <administrator@gsstvl.com>
2023-04-10 10:16:57 +0000

    7438 - Add column in transaction table

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_payment_processing_details_ui/f_payment_processing_details_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_payment_processing_details_ui/f_payment_processing_details_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29027 
+Build ID        : 29049 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-04 14:17 PM 
+Modified Date   : 2023-Apr-10 10:15 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_verification
--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_returns/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_returns/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29011 
+Build ID        : 29049 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-03 13:21 PM 
+Modified Date   : 2023-Apr-10 10:15 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_rejected_returns

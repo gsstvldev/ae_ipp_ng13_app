5c2b685572fbb2a38011ee4fc6ce65801736dbe5
administrator <administrator@gsstvl.com>
2023-04-17 08:02:06 +0000

    7483 - Query changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_payments/p_close_layout/p_close_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_rejected_payments/p_close_layout/p_close_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29075 
+Build ID        : 29059 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-11 9:52 AM 
+Modified Date   : 2023-Apr-17 7:52 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_rejected_payments

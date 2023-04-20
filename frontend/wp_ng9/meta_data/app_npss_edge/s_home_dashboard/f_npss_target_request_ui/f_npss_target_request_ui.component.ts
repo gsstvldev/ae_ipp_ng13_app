aad77bedf8e9aa9e29622a42ef04ae1e810d61c3
administrator <administrator@gsstvl.com>
2023-04-20 14:21:45 +0000

    7537 - action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29160 
+Build ID        : 29178 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-20 13:53 PM 
+Modified Date   : 2023-Apr-20 14:21 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

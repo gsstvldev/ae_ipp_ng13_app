c338868b290010ad827f4ac1f0254b6366d6a16c
administrator <administrator@gsstvl.com>
2023-04-21 07:22:25 +0000

    7543 - Home Dashboard Query changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_process_log_list_history_layout/p_process_log_list_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_process_log_list_history_layout/p_process_log_list_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29179 
+Build ID        : 29180 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-20 14:48 PM 
+Modified Date   : 2023-Apr-21 7:21 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

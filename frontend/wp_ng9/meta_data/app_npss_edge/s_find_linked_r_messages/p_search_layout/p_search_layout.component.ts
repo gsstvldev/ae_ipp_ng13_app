20d49cd43d346a316f5165e5257ae29b8c2e7789
administrator <administrator@gsstvl.com>
2023-04-21 14:48:54 +0000

    7558 - svm setup

--- frontend/wp_ng9/meta_data/app_npss_edge/s_find_linked_r_messages/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_find_linked_r_messages/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29202 
+Build ID        : 29212 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-21 14:6 PM 
+Modified Date   : 2023-Apr-21 14:48 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_find_linked_r_messages

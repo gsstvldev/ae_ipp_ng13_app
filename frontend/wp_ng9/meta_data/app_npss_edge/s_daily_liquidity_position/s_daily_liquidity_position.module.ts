20767a91df03af4be926b7f108bc30a10cf58b98
administrator <administrator@gsstvl.com>
2023-04-29 09:22:12 +0000

    7685 - column length changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_daily_liquidity_position/s_daily_liquidity_position.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_daily_liquidity_position/s_daily_liquidity_position.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29203 
+Build ID        : 29372 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-21 11:19 AM 
+Modified Date   : 2023-Apr-29 9:19 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_daily_liquidity_position
--- frontend/wp_ng9/meta_data/app_npss_edge/s_payment_list/s_payment_list.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_payment_list/s_payment_list.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29302 
+Build ID        : 29372 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-26 11:27 AM 
+Modified Date   : 2023-Apr-29 9:19 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_payment_list

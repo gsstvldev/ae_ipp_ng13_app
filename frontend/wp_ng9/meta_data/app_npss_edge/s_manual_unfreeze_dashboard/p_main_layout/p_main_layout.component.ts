ecc38a38d1b6ab2392ea1b2d4580c25b3e4722c4
administrator <administrator@gsstvl.com>
2023-05-11 12:58:28 +0000

    7792 - QRY CHANGES

--- frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29518 
+Build ID        : 29522 
 Modified By     : Admin 
-Modified Date   : 2023-May-11 11:41 AM 
+Modified Date   : 2023-May-11 12:57 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_manual_unfreeze_dashboard

e0812c802e5df4194c7027e119fef2c6427c8001
administrator <administrator@gsstvl.com>
2023-05-12 05:29:40 +0000

    7793 - PM Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/p_view_history_layout/p_view_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/p_view_history_layout/p_view_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29522 
+Build ID        : 29524 
 Modified By     : Admin 
-Modified Date   : 2023-May-11 12:57 PM 
+Modified Date   : 2023-May-11 13:37 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_manual_unfreeze_dashboard

77fa2c9930570121a4899132231816e7ff457eb4
administrator <administrator@gsstvl.com>
2023-05-16 10:27:11 +0000

    7821 - process log desc

--- frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/s_manual_unfreeze_dashboard.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_manual_unfreeze_dashboard/s_manual_unfreeze_dashboard.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29558 
+Build ID        : 29560 
 Modified By     : Admin 
-Modified Date   : 2023-May-16 9:57 AM 
+Modified Date   : 2023-May-16 10:26 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_manual_unfreeze_dashboard

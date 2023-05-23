a7db5a5470345f52837c3525891d56980237ee98
administrator <administrator@gsstvl.com>
2023-05-23 13:05:53 +0000

    7876 - Action changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29630 
+Build ID        : 29643 
 Modified By     : Admin 
-Modified Date   : 2023-May-23 12:33 PM 
+Modified Date   : 2023-May-23 13:5 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

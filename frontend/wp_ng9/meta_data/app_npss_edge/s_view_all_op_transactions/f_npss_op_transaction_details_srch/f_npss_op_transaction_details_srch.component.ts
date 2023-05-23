85aff150e2058f2f5058f4177f8aa55e662b6041
administrator <administrator@gsstvl.com>
2023-05-23 13:38:56 +0000

    7877 - Framework release

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_op_transaction_details_srch/f_npss_op_transaction_details_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_op_transaction_details_srch/f_npss_op_transaction_details_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29643 
+Build ID        : 29644 
 Modified By     : Admin 
-Modified Date   : 2023-May-23 13:5 PM 
+Modified Date   : 2023-May-23 13:38 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

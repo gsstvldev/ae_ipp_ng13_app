80db1caac758917872f6c75d838d0353c0852dec
administrator <administrator@gsstvl.com>
2023-05-24 07:30:16 +0000

    7879 - Corrections

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29644 
+Build ID        : 29645 
 Modified By     : Admin 
-Modified Date   : 2023-May-23 13:38 PM 
+Modified Date   : 2023-May-24 7:28 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_ip_transactions

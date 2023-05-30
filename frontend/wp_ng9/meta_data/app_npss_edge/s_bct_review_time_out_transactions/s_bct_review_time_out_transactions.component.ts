b34a7c65e9bfaa7e285854cfe767c4a7735c39f3
administrator <administrator@gsstvl.com>
2023-05-30 10:56:47 +0000

    7972 - 2610

--- frontend/wp_ng9/meta_data/app_npss_edge/s_bct_review_time_out_transactions/s_bct_review_time_out_transactions.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_review_time_out_transactions/s_bct_review_time_out_transactions.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29743 
+Build ID        : 29791 
 Modified By     : Admin 
-Modified Date   : 2023-May-29 6:8 AM 
+Modified Date   : 2023-May-30 10:55 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_bct_review_time_out_transactions

1c27282a1c584be984b95428e6db65db662daed7
administrator <administrator@gsstvl.com>
2023-05-31 08:15:19 +0000

    7994 -  Column Creation in req resp details table

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_transactions/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29791 
+Build ID        : 29818 
 Modified By     : Admin 
-Modified Date   : 2023-May-30 10:55 AM 
+Modified Date   : 2023-May-31 8:12 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_ip_transactions

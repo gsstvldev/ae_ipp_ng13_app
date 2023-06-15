de9702152ba817a11701496cbc028843aae90eef
administrator <administrator@gsstvl.com>
2023-06-15 10:03:08 +0000

    8274 - View process log list query changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_view_tran_detail_layout/p_view_tran_detail_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_view_tran_detail_layout/p_view_tran_detail_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30175 
+Build ID        : 30184 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-15 5:11 AM 
+Modified Date   : 2023-Jun-15 10:1 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_arc_transactions

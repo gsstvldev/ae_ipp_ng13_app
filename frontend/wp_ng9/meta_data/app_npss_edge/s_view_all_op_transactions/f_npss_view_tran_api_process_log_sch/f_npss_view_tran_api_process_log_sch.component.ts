c18ef7f32d70bb827e14bd14831455e8be85d910
administrator <administrator@gsstvl.com>
2023-06-16 10:53:38 +0000

    8302 - CHANGES

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_view_tran_api_process_log_sch/f_npss_view_tran_api_process_log_sch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_view_tran_api_process_log_sch/f_npss_view_tran_api_process_log_sch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30224 
+Build ID        : 30225 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-16 10:17 AM 
+Modified Date   : 2023-Jun-16 10:52 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

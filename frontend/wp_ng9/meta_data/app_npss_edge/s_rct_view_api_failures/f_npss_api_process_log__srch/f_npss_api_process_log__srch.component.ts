4bcf2748094c35916e38b7c693b6502e5df225ff
administrator <administrator@gsstvl.com>
2023-06-20 10:12:50 +0000

    8373 - WI2631

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_view_api_failures/f_npss_api_process_log__srch/f_npss_api_process_log__srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_view_api_failures/f_npss_api_process_log__srch/f_npss_api_process_log__srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30159 
+Build ID        : 30177 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-14 10:32 AM 
+Modified Date   : 2023-Jun-20 10:10 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_view_api_failures

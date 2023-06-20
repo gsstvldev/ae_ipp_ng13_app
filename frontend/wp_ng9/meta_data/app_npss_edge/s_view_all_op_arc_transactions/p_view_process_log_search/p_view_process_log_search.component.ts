36767d0f3c4cc66a1c69a7e9e1592854a024c78f
administrator <administrator@gsstvl.com>
2023-06-20 13:49:45 +0000

    8388 - To set Process name in MI Level for View all transacation screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_view_process_log_search/p_view_process_log_search.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_view_process_log_search/p_view_process_log_search.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30309 
+Build ID        : 30328 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-20 7:58 AM 
+Modified Date   : 2023-Jun-20 13:47 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_arc_transactions
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_navigation_ui/f_navigation_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_navigation_ui/f_navigation_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30177 
+Build ID        : 30328 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-20 10:10 AM 
+Modified Date   : 2023-Jun-20 13:47 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

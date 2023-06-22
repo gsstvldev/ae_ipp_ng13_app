2d407c262275f91428b8c1c0f4fd2cced167facc
administrator <administrator@gsstvl.com>
2023-06-22 10:46:47 +0000

    8441 - qryt

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_message_detail_action/f_message_detail_action.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_message_detail_action/f_message_detail_action.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30355 
+Build ID        : 30403 
 Modified By     : Admin 
-Modified Date   : 2023-Jun-21 11:1 AM 
+Modified Date   : 2023-Jun-22 10:44 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

eb6c3e3ee27dd9b2f5d6a8052f442ea3b014cb64
administrator <administrator@gsstvl.com>
2023-07-05 05:40:15 +0000

    8817 - LCA screen changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_lca_account_entries_exceptions/p_view_message_list_layout/p_view_message_list_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_lca_account_entries_exceptions/p_view_message_list_layout/p_view_message_list_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 30840 
+Build ID        : 30886 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-04 10:50 AM 
+Modified Date   : 2023-Jul-05 5:39 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_lca_account_entries_exceptions

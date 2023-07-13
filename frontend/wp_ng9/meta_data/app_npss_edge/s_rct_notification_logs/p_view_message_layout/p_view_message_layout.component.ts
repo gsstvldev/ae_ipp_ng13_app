da8a3f75544e2ca04178bef6025a5b094a6797c4
administrator <administrator@gsstvl.com>
2023-07-13 10:40:38 +0000

    8961 - Column creation in Npss_notification_logs

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_notification_logs/p_view_message_layout/p_view_message_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_notification_logs/p_view_message_layout/p_view_message_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31000 
+Build ID        : 31070 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-11 12:40 PM 
+Modified Date   : 2023-Jul-13 10:38 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_notification_logs

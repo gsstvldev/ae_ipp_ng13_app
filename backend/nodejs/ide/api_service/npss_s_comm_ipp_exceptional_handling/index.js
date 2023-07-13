ee8290671b8527985081485bab9fa176fc998570
administrator <administrator@gsstvl.com>
2023-07-13 11:29:49 +0000

    8964 - exception uetr

--- backend/nodejs/ide/api_service/npss_s_comm_ipp_exceptional_handling/index.js
+++ backend/nodejs/ide/api_service/npss_s_comm_ipp_exceptional_handling/index.js
@@ -10,7 +10,7 @@ app.post('/', function(appRequest, appResponse, next) {
     
 
 
-    /*  Created By :  Daseen
+    /*  Created By :   Daseen
   Created Date : 06/01/2023
   Modified By : Siva Harish
   Modified Date : 09/01/2023

4163711791b4f0db19d9186ac0012d0c254f6d62
administrator <administrator@gsstvl.com>
2023-07-17 07:15:54 +0000

    8986 - WI 2735 column creation

--- frontend/wp_ng9/meta_data/app_npss_edge/s_bct_file_upload/f_navigation/f_navigation.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_bct_file_upload/f_navigation/f_navigation.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31091 
+Build ID        : 31106 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-14 8:0 AM 
+Modified Date   : 2023-Jul-17 7:14 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_bct_file_upload

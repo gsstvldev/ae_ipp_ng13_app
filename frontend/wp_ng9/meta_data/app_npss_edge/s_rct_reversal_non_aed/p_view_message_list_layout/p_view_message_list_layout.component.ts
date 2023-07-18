a82cb53d771fdf184f46ef0f8c13ec5f13e48e48
administrator <administrator@gsstvl.com>
2023-07-18 12:57:51 +0000

    9001 - Label name changes for All inward and outward and dashboard and reports screens

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal_non_aed/p_view_message_list_layout/p_view_message_list_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal_non_aed/p_view_message_list_layout/p_view_message_list_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31109 
+Build ID        : 31119 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-17 12:26 PM 
+Modified Date   : 2023-Jul-18 12:55 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal_non_aed

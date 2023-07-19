db83cf1eacc7ab4a6d1bd333bc154ef095bc6dda
administrator <administrator@gsstvl.com>
2023-07-19 10:52:20 +0000

    9018 - npss commits

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_view_response_data_ui/f_npss_view_response_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_npss_view_response_data_ui/f_npss_view_response_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31119 
+Build ID        : 31140 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-18 12:55 PM 
+Modified Date   : 2023-Jul-19 10:36 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

ca981ba258867b1470502b1c837e3d18fb95abdf
administrator <administrator@gsstvl.com>
2023-07-21 11:57:42 +0000

    9051 - Test case for new setup

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_single_back_ui/f_single_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_investigation/f_single_back_ui/f_single_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31119 
+Build ID        : 31080 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-18 12:55 PM 
+Modified Date   : 2023-Jul-21 11:54 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_investigation

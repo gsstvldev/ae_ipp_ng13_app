82780cdd9bd76d694467e201f909752051629c56
administrator <administrator@gsstvl.com>
2023-07-21 15:14:24 +0000

    9057 - Live Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt052/p_message_data_layout/p_message_data_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt052/p_message_data_layout/p_message_data_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31189 
+Build ID        : 31195 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-21 13:49 PM 
+Modified Date   : 2023-Jul-21 15:12 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_camt052

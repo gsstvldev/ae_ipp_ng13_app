9e9a653dd6efb4e5d106ae93129a546819a00376
administrator <administrator@gsstvl.com>
2023-07-21 13:50:57 +0000

    9054 - Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31080 
+Build ID        : 31189 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-21 11:54 AM 
+Modified Date   : 2023-Jul-21 13:49 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt054/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt054/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 29130 
+Build ID        : 31189 
 Modified By     : Admin 
-Modified Date   : 2023-Apr-19 10:25 AM 
+Modified Date   : 2023-Jul-21 13:49 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_camt054

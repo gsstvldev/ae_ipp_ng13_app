8ad8b7b74fc130081a73eadcc68c3a1a780efef5
administrator <administrator@gsstvl.com>
2023-07-27 13:27:36 +0000

    9122 - Manual Initiation and Verification Screen CCD Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31124 
+Build ID        : 31276 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-27 12:32 PM 
+Modified Date   : 2023-Jul-27 13:21 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal

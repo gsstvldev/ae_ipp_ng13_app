b6c123d8e9c2df9ce1e049bec7d8db47efadded7
administrator <administrator@gsstvl.com>
2023-07-27 12:36:37 +0000

    9121 - Manual initiation and manual verification screen CCD changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_cbuae_exception/s_cbuae_exception.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_cbuae_exception/s_cbuae_exception.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31080 
+Build ID        : 31124 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-21 11:55 AM 
+Modified Date   : 2023-Jul-27 12:32 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_cbuae_exception
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_arc_transactions/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31119 
+Build ID        : 31124 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-18 12:56 PM 
+Modified Date   : 2023-Jul-27 12:33 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_arc_transactions

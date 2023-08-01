0d0363f0335a4833eff93460069a75738f06b6c9
administrator <administrator@gsstvl.com>
2023-08-01 11:24:50 +0000

    9170 - RCT Reversal screen changes for UAB

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal_non_aed/f_npss_reject_ui/f_npss_reject_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal_non_aed/f_npss_reject_ui/f_npss_reject_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 31292 
+Build ID        : 31193 
 Modified By     : Admin 
-Modified Date   : 2023-Jul-28 13:38 PM 
+Modified Date   : 2023-Aug-01 11:23 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal_non_aed

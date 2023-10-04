f6075afc3701b2525d9e2e49055fb691f76557f9
administrator <administrator@gsstvl.com>
2023-10-04 10:33:21 +0000

    9999 - Req to query changes in OP view all transaction screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32333 
+Build ID        : 32456 
 Modified By     : Admin 
-Modified Date   : 2023-Sep-21 10:15 AM 
+Modified Date   : 2023-Sep-29 13:12 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

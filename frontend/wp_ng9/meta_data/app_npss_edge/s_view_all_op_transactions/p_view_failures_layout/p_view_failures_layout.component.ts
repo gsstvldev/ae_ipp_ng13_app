371ce050300ac7781f91e39e614739836c745fb7
administrator <administrator@gsstvl.com>
2023-10-04 11:49:04 +0000

    10002 - Query changesss

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_failures_layout/p_view_failures_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_failures_layout/p_view_failures_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32456 
+Build ID        : 32520 
 Modified By     : Admin 
-Modified Date   : 2023-Sep-29 13:12 PM 
+Modified Date   : 2023-Oct-04 11:48 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

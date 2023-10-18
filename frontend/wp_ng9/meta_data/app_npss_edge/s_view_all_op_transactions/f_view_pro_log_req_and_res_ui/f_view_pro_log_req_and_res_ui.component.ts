c9210244bdf4ab5ef32b2e90ee96699f52ddc9a1
administrator <administrator@gsstvl.com>
2023-10-18 13:30:29 +0000

    10209 - changes in api query

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_view_pro_log_req_and_res_ui/f_view_pro_log_req_and_res_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_view_pro_log_req_and_res_ui/f_view_pro_log_req_and_res_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32804 
+Build ID        : 32813 
 Modified By     : Admin 
-Modified Date   : 2023-Oct-18 8:6 AM 
+Modified Date   : 2023-Oct-18 13:29 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

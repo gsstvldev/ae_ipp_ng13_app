ab42e91a9679875ef02d7bfa59a279712dc03618
administrator <administrator@gsstvl.com>
2023-10-26 13:58:05 +0000

    10273 - QUERY CHANGES FOR HD

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_process_log_list_history_layout/p_process_log_list_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_process_log_list_history_layout/p_process_log_list_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32903 
+Build ID        : 32912 
 Modified By     : Admin 
-Modified Date   : 2023-Oct-26 9:54 AM 
+Modified Date   : 2023-Oct-26 13:56 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

8eb4913865d664a1fb67f3a22da595b8c55a89d6
administrator <administrator@gsstvl.com>
2023-10-30 13:57:49 +0000

    10301 - API Queue Search Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32937 
+Build ID        : 32947 
 Modified By     : Admin 
-Modified Date   : 2023-Oct-30 7:31 AM 
+Modified Date   : 2023-Oct-30 13:55 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

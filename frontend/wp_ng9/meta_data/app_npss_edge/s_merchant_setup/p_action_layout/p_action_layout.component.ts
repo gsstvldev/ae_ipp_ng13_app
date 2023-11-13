ccda5895a5c58d7d01bf447e2cf9dfd33d85b72f
administrator <administrator@gsstvl.com>
2023-11-13 12:58:54 +0000

    10478 - WI 3131

--- frontend/wp_ng9/meta_data/app_npss_edge/s_merchant_setup/p_action_layout/p_action_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_merchant_setup/p_action_layout/p_action_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 28958 
+Build ID        : 33186 
 Modified By     : Admin 
-Modified Date   : 2023-Mar-29 13:6 PM 
+Modified Date   : 2023-Nov-13 12:57 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_merchant_setup
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/p_view_shop_layout/p_view_shop_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_merchant_details/p_view_shop_layout/p_view_shop_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32699 
+Build ID        : 33186 
 Modified By     : Admin 
-Modified Date   : 2023-Oct-13 6:28 AM 
+Modified Date   : 2023-Nov-13 12:57 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_merchant_details

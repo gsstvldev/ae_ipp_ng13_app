d5e46e5695e9d2386f13125bc2e4cf7013f28ae9
administrator <administrator@gsstvl.com>
2023-11-14 08:06:05 +0000

    10487 - changes in view dst name

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_reversal/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 33059 
+Build ID        : 33199 
 Modified By     : Admin 
-Modified Date   : 2023-Nov-06 14:1 PM 
+Modified Date   : 2023-Nov-14 8:4 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_reversal

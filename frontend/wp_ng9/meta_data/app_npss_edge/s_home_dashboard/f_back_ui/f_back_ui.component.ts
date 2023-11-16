4fc8d09fbe3e37a47e30d310522bebf768c89b6e
administrator <administrator@gsstvl.com>
2023-11-16 10:41:20 +0000

    10511 -  WI 3157 changes manual Initation and Manual Verification

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_back_ui/f_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_back_ui/f_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 32916 
+Build ID        : 33232 
 Modified By     : Admin 
-Modified Date   : 2023-Oct-27 14:55 PM 
+Modified Date   : 2023-Nov-16 10:39 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

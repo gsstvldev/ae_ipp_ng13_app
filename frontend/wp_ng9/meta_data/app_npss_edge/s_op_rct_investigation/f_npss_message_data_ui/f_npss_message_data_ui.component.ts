db001c6a4d6f376a9733478bc598850dfbeb88fd
administrator <administrator@gsstvl.com>
2023-11-16 12:40:38 +0000

    10514 - changes in manual initation and verification

--- frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_op_rct_investigation/f_npss_message_data_ui/f_npss_message_data_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 33232 
+Build ID        : 33235 
 Modified By     : Admin 
-Modified Date   : 2023-Nov-16 10:39 AM 
+Modified Date   : 2023-Nov-16 12:39 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_op_rct_investigation

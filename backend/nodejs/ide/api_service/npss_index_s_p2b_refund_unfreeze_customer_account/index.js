160740a2a8a003a2684a2ab4ee7f14c22ffa0604
administrator <administrator@gsstvl.com>
2023-12-11 12:30:37 +0000

    10762 - npss index p2b unfreeze

---
+++ backend/nodejs/ide/api_service/npss_index_s_p2b_refund_unfreeze_customer_account/index.js
@@ -0,0 +1,13 @@
+var $MODULEPATH = '../../node_modules/';
+var express = require('express');
+var Path = require('path');
+var $REFPATH = Path.join(__dirname, '../../torus-references/');
+
+var app = express.Router();
+
+app.post('/', function(appRequest, appResponse, next) {
+console.log("only for npss index exchange");
+
+});
+
+module.exports = app;

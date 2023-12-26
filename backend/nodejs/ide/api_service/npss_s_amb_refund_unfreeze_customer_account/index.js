0176b19460ad6261eb2ea41148bc21c0984c346b
administrator <administrator@gsstvl.com>
2023-12-26 07:14:38 +0000

    10938 - npss sch for refund unfreeze customer account ide

---
+++ backend/nodejs/ide/api_service/npss_s_amb_refund_unfreeze_customer_account/index.js
@@ -0,0 +1,13 @@
+var $MODULEPATH = '../../node_modules/';
+var express = require('express');
+var Path = require('path');
+var $REFPATH = Path.join(__dirname, '../../torus-references/');
+
+var app = express.Router();
+
+app.post('/', function(appRequest, appResponse, next) {
+console.log(`this ide is for a scheduler in amb refund unfreeze customer account`)
+
+});
+
+module.exports = app;

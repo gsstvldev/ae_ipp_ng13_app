8d915969bcbfc188bc9f229a4cbd45e81713b1ba
administrator <administrator@gsstvl.com>
2024-02-20 09:34:24 +0000

    11353 - commits for setup

--- frontend/wp_ng9/meta_data/app_npss_edge/s_outward_transactions_details/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_outward_transactions_details/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 34442 
+Build ID        : 34446 
 Modified By     : Admin 
-Modified Date   : 2024-Feb-20 7:40 AM 
+Modified Date   : 2024-Feb-20 9:32 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_outward_transactions_details

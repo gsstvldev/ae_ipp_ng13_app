83ee334a73d20fd48244d6e52f2599571ad44c49
administrator <administrator@gsstvl.com>
2024-03-14 10:36:07 +0000

    11593 - OP Wiring Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_payment_status_report/f_npss_payment_status_report_srch/f_npss_payment_status_report_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_payment_status_report/f_npss_payment_status_report_srch/f_npss_payment_status_report_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 34763 
+Build ID        : 34764 
 Modified By     : Admin 
-Modified Date   : 2024-Mar-14 9:56 AM 
+Modified Date   : 2024-Mar-14 10:29 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_payment_status_report

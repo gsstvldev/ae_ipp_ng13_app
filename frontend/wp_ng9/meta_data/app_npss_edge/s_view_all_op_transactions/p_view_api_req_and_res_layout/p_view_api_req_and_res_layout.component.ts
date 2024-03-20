b1e36bd2b4e33e6c41c131e6ac9d0af14a3d6404
administrator <administrator@gsstvl.com>
2024-03-20 04:46:47 +0000

    11641 - View All OP Tran Query Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_api_req_and_res_layout/p_view_api_req_and_res_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_api_req_and_res_layout/p_view_api_req_and_res_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 34815 
+Build ID        : 34825 
 Modified By     : Admin 
-Modified Date   : 2024-Mar-19 10:30 AM 
+Modified Date   : 2024-Mar-20 4:45 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

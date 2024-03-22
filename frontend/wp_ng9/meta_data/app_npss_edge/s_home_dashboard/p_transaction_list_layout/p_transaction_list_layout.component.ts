72d33101320f846a783f1e48722440dffee2c499
administrator <administrator@gsstvl.com>
2024-03-22 07:56:30 +0000

    11665 - Home Dashboard Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_transaction_list_layout/p_transaction_list_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_transaction_list_layout/p_transaction_list_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 34842 
+Build ID        : 34849 
 Modified By     : Admin 
-Modified Date   : 2024-Mar-22 5:52 AM 
+Modified Date   : 2024-Mar-22 6:57 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

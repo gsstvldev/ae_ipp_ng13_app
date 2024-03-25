7b9f95b0e0db49d392f1a32d8d449478cd0cf828
administrator <administrator@gsstvl.com>
2024-03-25 07:51:40 +0000

    11684 - Action level changes Audit Module

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_arc_transactions/f_message_detail_action/f_message_detail_action.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_ip_arc_transactions/f_message_detail_action/f_message_detail_action.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 34784 
+Build ID        : 34878 
 Modified By     : Admin 
-Modified Date   : 2024-Mar-15 11:48 AM 
+Modified Date   : 2024-Mar-25 7:50 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_ip_arc_transactions

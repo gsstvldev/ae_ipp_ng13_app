36ce77ba25332f260e4a9742f4f00ed960c4e09b
administrator <administrator@gsstvl.com>
2024-04-09 10:45:29 +0000

    11851 - npss changes in module.ts

--- frontend/wp_ng9/custom_widget/npss_cs_credit_debit_report/npss_cs_credit_debit_report.module.ts
+++ frontend/wp_ng9/custom_widget/npss_cs_credit_debit_report/npss_cs_credit_debit_report.module.ts
@@ -2,8 +2,10 @@
     import { NgModule } from '@angular/core';
     import { CommonModule } from '@angular/common';
     import { npss_cs_credit_debit_reportComponent } from './npss_cs_credit_debit_report.component'; 
+    import { PdfViewerModule } from 'ng2-pdf-viewer';
+
     @NgModule({
-        imports: [CommonModule],
+        imports: [CommonModule,PdfViewerModule],
         exports:[npss_cs_credit_debit_reportComponent],
         declarations: [npss_cs_credit_debit_reportComponent],
         providers:[]

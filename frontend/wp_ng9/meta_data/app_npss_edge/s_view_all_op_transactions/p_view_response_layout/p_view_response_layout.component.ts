02bbecad887830b853d7ab22df5b214467d795cd
administrator <administrator@gsstvl.com>
2024-04-16 06:18:03 +0000

    11887 - View all op Transaction add transaction set

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_response_layout/p_view_response_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/p_view_response_layout/p_view_response_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35161 
+Build ID        : 35162 
 Modified By     : Admin 
-Modified Date   : 2024-Apr-16 5:32 AM 
+Modified Date   : 2024-Apr-16 6:14 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

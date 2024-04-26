413a868ae1a64ef20987b7c95c6f09d5e49c4100
administrator <administrator@gsstvl.com>
2024-04-26 13:06:41 +0000

    11959 - Action level chages home dashboard

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_message_back_ui/f_message_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_message_back_ui/f_message_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35261 
+Build ID        : 35265 
 Modified By     : Admin 
-Modified Date   : 2024-Apr-26 12:26 PM 
+Modified Date   : 2024-Apr-26 13:5 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

8d23bd64b2d01ea0636a9d96a5d022a0676ab939
administrator <administrator@gsstvl.com>
2024-05-09 05:10:12 +0000

    12036 -  changes for report changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_report_viewer/s_report_viewer.routing.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_report_viewer/s_report_viewer.routing.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35344 
+Build ID        : 35350 
 Modified By     : Admin 
-Modified Date   : 2024-May-08 13:46 PM 
+Modified Date   : 2024-May-09 5:9 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_report_viewer

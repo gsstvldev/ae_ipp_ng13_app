d46c4355ec3b6d8d369411dad720f237023752aa
administrator <administrator@gsstvl.com>
2024-05-13 11:04:23 +0000

    12051 - debit Report Cdb Change

--- frontend/wp_ng9/meta_data/app_npss_edge/s_debit_advice_report/f_debit_advice_report_srch/f_debit_advice_report_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_debit_advice_report/f_debit_advice_report_srch/f_debit_advice_report_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35197 
+Build ID        : 35364 
 Modified By     : Admin 
-Modified Date   : 2024-Apr-23 12:19 PM 
+Modified Date   : 2024-May-13 11:2 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_debit_advice_report

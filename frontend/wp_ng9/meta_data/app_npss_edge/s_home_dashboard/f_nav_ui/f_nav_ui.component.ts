f410ee0febe052da6c48c3ee7060eddfeba988b9
administrator <administrator@gsstvl.com>
2024-05-15 11:10:47 +0000

    12070 - home dashboard Screen

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_nav_ui/f_nav_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_nav_ui/f_nav_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35381 
+Build ID        : 35386 
 Modified By     : Admin 
-Modified Date   : 2024-May-15 10:38 AM 
+Modified Date   : 2024-May-15 11:9 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

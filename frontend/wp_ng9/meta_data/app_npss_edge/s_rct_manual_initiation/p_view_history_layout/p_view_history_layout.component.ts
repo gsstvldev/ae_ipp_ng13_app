7c27730678f0a1492032ba6a41cd3dea40270505
administrator <administrator@gsstvl.com>
2024-05-15 10:39:52 +0000

    12069 - Change Query home dashboard

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_view_history_layout/p_view_history_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_view_history_layout/p_view_history_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35367 
+Build ID        : 35384 
 Modified By     : Admin 
-Modified Date   : 2024-May-14 7:54 AM 
+Modified Date   : 2024-May-15 10:34 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_initiation

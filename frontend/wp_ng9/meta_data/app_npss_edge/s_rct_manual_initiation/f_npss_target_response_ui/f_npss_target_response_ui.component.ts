7daaaf23ba305af95489163bad7ed3109474e1fa
administrator <administrator@gsstvl.com>
2024-05-16 06:01:40 +0000

    12079 - Manual initiation screen changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/f_npss_target_response_ui/f_npss_target_response_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35384 
+Build ID        : 35398 
 Modified By     : Admin 
-Modified Date   : 2024-May-15 10:34 AM 
+Modified Date   : 2024-May-16 6:0 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_initiation

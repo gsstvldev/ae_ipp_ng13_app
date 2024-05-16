87d81c354c1e1228761eccbc44e68fa4062853f9
administrator <administrator@gsstvl.com>
2024-05-16 09:54:20 +0000

    12088 - change for MV

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_initiation/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35398 
+Build ID        : 35411 
 Modified By     : Admin 
-Modified Date   : 2024-May-16 6:0 AM 
+Modified Date   : 2024-May-16 9:53 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_initiation
--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_npss_manual_verification_srch/f_npss_manual_verification_srch.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_npss_manual_verification_srch/f_npss_manual_verification_srch.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35404 
+Build ID        : 35411 
 Modified By     : Admin 
-Modified Date   : 2024-May-16 8:9 AM 
+Modified Date   : 2024-May-16 9:53 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_verification

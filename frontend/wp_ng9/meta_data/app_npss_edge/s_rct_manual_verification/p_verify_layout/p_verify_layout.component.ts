45146b103fed4df12922320d6c2f56c7231a8c01
administrator <administrator@gsstvl.com>
2024-05-21 13:40:59 +0000

    12145 - Manual Verification filter status

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/p_verify_layout/p_verify_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/p_verify_layout/p_verify_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35507 
+Build ID        : 35509 
 Modified By     : Admin 
-Modified Date   : 2024-May-21 12:46 PM 
+Modified Date   : 2024-May-21 13:39 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_verification

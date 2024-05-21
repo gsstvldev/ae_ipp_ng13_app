53b996170224c3c59c1576235bf976918a15088e
administrator <administrator@gsstvl.com>
2024-05-21 06:33:08 +0000

    12132 - Home dashboard OFDL

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_message_back_ui/f_message_back_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/f_message_back_ui/f_message_back_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35386 
+Build ID        : 35491 
 Modified By     : Admin 
-Modified Date   : 2024-May-15 11:9 AM 
+Modified Date   : 2024-May-21 6:31 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard

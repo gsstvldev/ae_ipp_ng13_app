abdd2dab6ea600b6cb1967791df785121aa1f063
administrator <administrator@gsstvl.com>
2024-05-21 14:11:11 +0000

    12146 - query changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_search_info_ui/f_search_info_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_rct_manual_verification/f_search_info_ui/f_search_info_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35509 
+Build ID        : 35510 
 Modified By     : Admin 
-Modified Date   : 2024-May-21 13:39 PM 
+Modified Date   : 2024-May-21 14:10 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_rct_manual_verification

57cef1a335c84a53b0f267db91c0507ff1527e1c
administrator <administrator@gsstvl.com>
2024-06-04 09:40:10 +0000

    12219 - API Queue and process log list changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_view_pro_log_req_and_res_ui/f_view_pro_log_req_and_res_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_all_op_transactions/f_view_pro_log_req_and_res_ui/f_view_pro_log_req_and_res_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35484 
+Build ID        : 35623 
 Modified By     : Admin 
-Modified Date   : 2024-May-20 13:45 PM 
+Modified Date   : 2024-Jun-04 9:38 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_all_op_transactions

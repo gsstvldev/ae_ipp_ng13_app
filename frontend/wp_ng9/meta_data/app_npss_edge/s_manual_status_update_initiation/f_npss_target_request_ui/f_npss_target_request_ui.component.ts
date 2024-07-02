1a385de191669a43500e76ff5759eedcc5382cad
administrator <administrator@gsstvl.com>
2024-07-02 05:05:16 +0000

    12428 - Inward Payment Reversal Report Creation

--- frontend/wp_ng9/meta_data/app_npss_edge/s_manual_status_update_initiation/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_manual_status_update_initiation/f_npss_target_request_ui/f_npss_target_request_ui.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35906 
+Build ID        : 35909 
 Modified By     : Admin 
-Modified Date   : 2024-Jul-01 11:27 AM 
+Modified Date   : 2024-Jul-01 12:58 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_manual_status_update_initiation
--- frontend/wp_ng9/meta_data/app_npss_edge/s_outward_transactions_details/p_search_layout/p_search_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_outward_transactions_details/p_search_layout/p_search_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35872 
+Build ID        : 35909 
 Modified By     : Admin 
-Modified Date   : 2024-Jun-27 12:29 PM 
+Modified Date   : 2024-Jul-01 12:58 PM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_outward_transactions_details

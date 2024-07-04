37096884398c7c4c9fb69d36827cddcc86f45fff
manim <subramanian04051@gmail.com>
2024-07-04 15:45:12 +0530

    debit report  qry changes

--- backend/nodejs/ide/api_service/npss_cs_credit_debit_report/index.js
+++ backend/nodejs/ide/api_service/npss_cs_credit_debit_report/index.js
@@ -12,10 +12,10 @@ app.post('/', function(appRequest, appResponse, next) {
 
 
 
-
     /*  Created By : Subramanian
     Created Date : 8/4/2024
     For: Credit Debit jasper report replacement
+    for query change 04-07-2024 by renga
     
      
     */
@@ -69,6 +69,13 @@ app.post('/', function(appRequest, appResponse, next) {
                                 arrTranID = [params.Tran_Id.toString()];
                             }
                             tranid = '(' + "'" + arrTranID.toString().split(',').join("','") + "'" + ')'; */
+                            var schema;
+                            if (params.db_type == 'LIVE') {
+                                schema = '<tran_db>'
+                            } else {
+                                schema = '<arc_tran_db>'
+                            }
+
                             var Taketran = '', cond_params = '', operator = '', value = ''
                             params.search_params.forEach((val) => {
                                 if (val.OPERATOR == '=' || val.OPERATOR.toLowerCase().includes('equal')) {
@@ -84,196 +91,187 @@ app.post('/', function(appRequest, appResponse, next) {
                             if (params.screenName.includes('debit')) {
 
                                 Taketran = `select
-distinct uetr as UETR,
-Debtor_Name as  ordering_customer_name,
-Debtor_Account as  ordering_account,
-Creditor_Name as  Beneficiary_Customer_Name,
-Creditor_Account as  Beneficiary_Customer_IBAN,
-dr_sort_code as  Beneficiary_Bank_Code,
-tran_ref_id as  Transaction_Ref,
-TRANSACTION_AMOUNT_RANGE as  Amount_AED,
-categorypurpose as  Transaction_Code,
-created_date as  Received_Time,
-STATUS as Status,
-payment_endtoend_id as  End_to_End_ID,
-
-tran_ref_id as  Transaction_Reference,
-otherreference as  Other_Reference,
-
-SOURCE_CHANNEL,
-
-STATUS,
-
-clrsysref,
-BENEFICIARY_BANK,
-End_to_End_ID,
-T24_FT_REFERENCE_NUMBER,
-process_type,
-API_Success_Failure,
-Purpose_codes,
-channel_refno,
-account_number,
-coalesce(Split_part(report_icon_data,
-'base64',
-2),
-'') report_icon_data
+	distinct uetr,
+	Debtor_Name,
+	 Debtor_Account,
+	 Creditor_Name,
+	 Creditor_Account,
+	 dr_sort_code,
+	 cr_sort_code,
+	 TRANSACTION_AMOUNT_RANGE,
+	 categorypurpose,
+	 created_date,
+	 CREATEDDATE,
+	payment_endtoend_id,
+	SOURCE_CHANNEL,
+	tran_ref_id,
+	STATUS,
+	otherreference,
+	clrsysref,
+	BENEFICIARY_BANK,
+	End_to_End_ID,
+	T24_FT_REFERENCE_NUMBER,
+	process_type,
+	API_Success_Failure,
+	Purpose_codes,
+	channel_refno,
+	account_number,
+	coalesce(Split_part(report_icon_data,'base64',2),'') report_icon_data
 from
-(
-select
-    NT.dbtr_acct_name as Debtor_Name,
-    nt.dbtr_iban as Debtor_Account,
-    nt.cdtr_acct_name,
-    NT.cdtr_iban as Creditor_Account,
-    nt.dr_sort_code,
-    nt.intrbk_sttlm_amnt as TRANSACTION_AMOUNT_RANGE,
-    case
-        when nt.process_group = 'MANUAL' then nt.instruction_id
-        when nt.process_group = 'P2P' then nt.remittance_info
-        when nt.process_group = 'P2B' then nt.remittance_info
-        when nt.process_group = 'IBAN' then nt.remittance_info
-        when nt.process_group = 'BCT' then nt.remittance_info
-        when nt.process_group = 'RCT' then nt.remittance_info
-    end as otherreference,
-    case
-        when nt.process_group = 'MANUAL' then nt.category_purpose_prty
-        when nt.process_group = 'IBAN' then nt.category_purpose
-        when nt.process_group = 'P2P' then nt.category_purpose
-        when nt.process_group = 'P2B' then nt.category_purpose
-        when nt.process_group = 'BCT' then nt.category_purpose
-        when nt.process_group = 'RCT' then nt.category_purpose
-    end as categorypurpose,
-    NT.cdtr_acct_name as Creditor_Name,
-    NT.channel_refno,
-    NT.channel_id as SOURCE_CHANNEL,
-    hdr_created_date as CREATEDDATE,
-    nt.CREATED_DATE,
-    nt.payment_endtoend_id,
-    nt.uetr,
-    nt.tran_ref_id,
-    nt.clrsysref,
-    nt.TENANT_ID,
-    cmb.bank_name as BENEFICIARY_BANK,
-    nt.process_type,
-    nt.PAYMENT_ENDTOEND_ID as End_to_End_ID,
-    L.report_icon_data,
-    nt.STATUS,
-    nt.process_status as API_Success_Failure,
-    nt.department_code as DEPARTMENT_NAME,
-    nt.VALUE_DATE,
-    NT.ext_purpose_code as Purpose_codes,
-    case
-        when ntpl.process_name = 'OP Debit Posting'
-        and nt.process_group = 'P2P' then ntpl.process_ref_no
-        when ntpl.process_name = 'Fund AUTH Posting'
-        and nt.process_group = 'P2B' then ntpl.process_ref_no
-        when ntpl.process_name = 'Manual Fund AUTH Posting'
-        and nt.process_group = 'MANUAL' then ntpl.process_ref_no
-        when nt.process_group = 'IBAN' then nt.tran_ref_id
-        when nt.process_group = 'BCT' then nt.tran_ref_id
-        when nt.process_group = 'RCT' then nt.tran_ref_id
-    end as T24_FT_REFERENCE_NUMBER,
-    ntpl.PROCESS_NAME,
-    cnca.account_number
-from
-    npss_transactions nt
-inner join npss_trn_process_log ntpl on
-    nt.UETR = ntpl.UETR
-left join core_nc_cbs_accounts cnca on
-    nt.dbtr_iban = cnca.alternate_account_id
-left join CORE_RPT_LOGO_SETUP L on
-    L.tenant_id = NT.TENANT_ID
-left join core_member_banks cmb on
-    cmb.bic_code = nt.cr_sort_code
-    and cmb.NEED_SYNC = 'Y' )v
+	(
+	select
+		NT.dbtr_acct_name as Debtor_Name,
+		nt.dbtr_iban as Debtor_Account,
+		nt.cdtr_acct_name,
+		NT.cdtr_iban as Creditor_Account,
+		nt.dr_sort_code,
+		nt.cr_sort_code,
+		nt.intrbk_sttlm_amnt as TRANSACTION_AMOUNT_RANGE,
+		case
+			when nt.process_group = 'MANUAL' then nt.instruction_id
+			when nt.process_group = 'P2P' then nt.remittance_info
+			when nt.process_group = 'P2B' then nt.remittance_info
+			when nt.process_group = 'IBAN' then nt.remittance_info
+			when nt.process_group = 'BCT' then nt.remittance_info
+			when nt.process_group = 'RCT' then nt.remittance_info
+		end as otherreference,
+		case
+			when nt.process_group = 'MANUAL' then nt.category_purpose_prty
+			when nt.process_group = 'IBAN' then nt.category_purpose
+			when nt.process_group = 'P2P' then nt.category_purpose
+			when nt.process_group = 'P2B' then nt.category_purpose
+			when nt.process_group = 'BCT' then nt.category_purpose
+			when nt.process_group = 'RCT' then nt.category_purpose
+		end as categorypurpose,
+		NT.cdtr_acct_name as Creditor_Name,
+		NT.channel_refno,
+		NT.channel_id as SOURCE_CHANNEL,
+		hdr_created_date as CREATEDDATE,
+		nt.CREATED_DATE,
+		nt.payment_endtoend_id,
+		nt.uetr,
+		nt.tran_ref_id,
+		nt.clrsysref,
+		nt.TENANT_ID,
+		cmb.bank_name as BENEFICIARY_BANK,
+		nt.process_type,
+		nt.PAYMENT_ENDTOEND_ID as End_to_End_ID,
+		L.report_icon_data,
+		nt.STATUS,
+		nt.process_status as API_Success_Failure,
+		nt.department_code,
+		nt.VALUE_DATE,
+		NT.ext_purpose_code as Purpose_codes,
+		case
+			when ntpl.process_name = 'OP Debit Posting'
+			and nt.process_group = 'P2P' then ntpl.process_ref_no
+			when ntpl.process_name = 'Fund AUTH Posting'
+			and nt.process_group = 'P2B' then ntpl.process_ref_no
+			when ntpl.process_name = 'Manual Fund AUTH Posting'
+			and nt.process_group = 'MANUAL' then ntpl.process_ref_no
+			when nt.process_group = 'IBAN' then nt.tran_ref_id
+			when nt.process_group = 'BCT' then nt.tran_ref_id
+			when nt.process_group = 'RCT' then nt.tran_ref_id
+		end as T24_FT_REFERENCE_NUMBER,
+		ntpl.PROCESS_NAME,
+		cnca.account_number
+	from
+		${schema}.npss_transactions nt
+	inner join ${schema}.npss_trn_process_log ntpl on
+		nt.UETR = ntpl.UETR
+	inner join CORE_SYSTEM_EXTN CSE on
+		CSE.department_code = nt.DEPARTMENT_CODE
+	left join core_nc_cbs_accounts cnca on
+		nt.dbtr_iban = cnca.alternate_account_id
+	left join CORE_RPT_LOGO_SETUP L on
+		L.tenant_id = NT.TENANT_ID
+	left join core_member_banks cmb on
+		cmb.bic_code = nt.cr_sort_code
+		and cmb.NEED_SYNC = 'Y'
+			)v
 where
-process_type = 'OP'  ${cond_params}`
+	process_type = 'OP'  ${cond_params}`
                             }
                             else {
-                                Taketran = `select
-                            distinct uetr as UETR,
-                            Creditor_Name as  Beneficiary_Customer_Name,
-                            Creditor_Account as  Benificiary_Account,
-                            account_number as  Beneficiary_Customer_IBAN,
-                            Debtor_Name as  Ordering_Customer_Name,
-                            dr_sort_code as  Ordering_Bank_Code,
-                            Debtor_Account as Ordering_Customer_IBAN,
-                            tran_ref_id as Transaction_ref,
-                            TRANSACTION_AMOUNT_RANGE as Amount_Aed,
-                            category_purpose_prty as Transaction_code,
-                            created_date as  Received_Time,
-                            status as  Status,
-                            payment_endtoend_id as end_to_end_id,
-                            tran_ref_id as  Transaction_Reference,
-                            clrsysref as  Clearing_Reference,
-                            SENDER_BANK,
-                            remittance_info,
-                            tran_ref_id,
-                            tenant_id,
-                            process_type,
-                            DEPARTMENT_NAME,
-                            
-                            T24_FT_REFERENCE_NUMBER,
-                            
-                            coalesce(Split_part(report_icon_data,
-                            'base64',
-                            2),
-                            '') report_icon_data
-                        from
-                            (
-                            select
-                                NT.cdtr_acct_name as Creditor_Name,
-                                NT.cdtr_iban as Creditor_Account,
-                                NT.dbtr_acct_name as Debtor_Name,
-                                nt.dr_sort_code,
-                                NT.dbtr_iban as Debtor_Account,
-                                NT.intrbk_sttlm_amnt as TRANSACTION_AMOUNT_RANGE,
-                                nt.department_code as DEPARTMENT_NAME,
-                                nt.category_purpose_prty,
-                                cmb.bank_name as SENDER_BANK,
-                                hdr_created_date as CREATEDDATE,
-                                nt.CREATED_DATE,
-                                nt.tenant_id,
-                                nt.uetr,
-                                nt.tran_ref_id,
-                                nt.clrsysref,
-                                nt.remittance_info,
-                                nt.process_type,
-                                L.report_icon_data,
-                                ntpl.process_name,
-                                NT.process_status,
-                                NT.status,
-                                NT.value_date,
-                                ntpl.T24_RETURN_CODE,
-                                NT.payment_endtoend_id,
-                                ntpl.process_ref_no as T24_FT_REFERENCE_NUMBER,
-                                cnca.account_number
-                            from
-                                npss_transactions nt
-                            inner join npss_trn_process_log ntpl on
-                                nt.UETR = ntpl.UETR
-                            left join core_nc_cbs_accounts cnca on
-                                nt.cdtr_iban = cnca.alternate_account_id
-                            left join CORE_RPT_LOGO_SETUP L on
-                                L.tenant_id = NT.TENANT_ID
-                            left join core_member_banks cmb on
-                                cmb.bic_code = nt.cr_sort_code
-                                and cmb.NEED_SYNC = 'Y'
-                            where
-                                ntpl.process_name = 'Posting Enquiry'
-                                and ntpl.status = 'IP_RCT_POSTING_SUCCESS'
-                                or ntpl.process_name = 'Inward Credit Posting'
-                                and ntpl.status = 'IP_BCT_POSTING_SUCCESS'
-                                or ntpl.process_name = 'Inward Credit Posting'
-                                and ntpl.status = 'IP_RCT_POSTING_SUCCESS' )v
-                        where
-                            process_type = 'IP'  ${cond_params} `
+                                Taketran = ` SELECT DISTINCT UETR,
+	CREDITOR_NAME,
+	CREDITOR_ACCOUNT,
+	DEBTOR_NAME,
+	DR_SORT_CODE,
+	DEBTOR_ACCOUNT,
+	TRANSACTION_AMOUNT_RANGE,
+	CATEGORY_PURPOSE_PRTY,
+	CREATED_DATE,
+	CREATEDDATE,
+	SENDER_BANK,
+	REMITTANCE_INFO,
+	TRAN_REF_ID,
+	CLRSYSREF,
+	STATUS,
+	PROCESS_TYPE,
+	DEPARTMENT_CODE,
+	T24_FT_REFERENCE_NUMBER,
+	ACCOUNT_NUMBER,
+	COALESCE(SPLIT_PART(REPORT_ICON_DATA,'base64',2),'') REPORT_ICON_DATA
+FROM
+	(SELECT NT.CDTR_ACCT_NAME AS CREDITOR_NAME,
+			NT.CDTR_IBAN AS CREDITOR_ACCOUNT,
+			NT.DBTR_ACCT_NAME AS DEBTOR_NAME,
+			NT.DR_SORT_CODE,
+			NT.DBTR_IBAN AS DEBTOR_ACCOUNT,
+			NT.INTRBK_STTLM_AMNT AS TRANSACTION_AMOUNT_RANGE,
+			NT.DEPARTMENT_CODE,
+			NT.CATEGORY_PURPOSE_PRTY,
+			CMB.BANK_NAME AS SENDER_BANK,
+			HDR_CREATED_DATE AS CREATEDDATE,
+			NT.CREATED_DATE,
+			NT.PAYMENT_ENDTOEND_ID,
+			NT.UETR,
+			NT.TRAN_REF_ID,
+			NT.CLRSYSREF,
+			NT.REMITTANCE_INFO,
+			NT.PROCESS_TYPE,
+			L.REPORT_ICON_DATA,
+			NTPL.PROCESS_NAME,
+			NT.PROCESS_STATUS,
+			NT.STATUS,
+			NT.VALUE_DATE,
+			NTPL.T24_RETURN_CODE,
+			NTPL.PROCESS_REF_NO AS T24_FT_REFERENCE_NUMBER,
+			CNCA.ACCOUNT_NUMBER
+		FROM ${schema}.NPSS_TRANSACTIONS NT
+		INNER JOIN ${schema}.NPSS_TRN_PROCESS_LOG NTPL ON NT.UETR = NTPL.UETR
+		INNER JOIN CORE_SYSTEM_EXTN CSE ON CSE.DEPARTMENT_CODE = NT.DEPARTMENT_CODE
+		LEFT JOIN CORE_NC_CBS_ACCOUNTS CNCA ON NT.CDTR_IBAN = CNCA.ALTERNATE_ACCOUNT_ID
+		LEFT JOIN CORE_RPT_LOGO_SETUP L ON L.TENANT_ID = NT.TENANT_ID
+		LEFT JOIN CORE_MEMBER_BANKS CMB ON CMB.BIC_CODE = NT.CR_SORT_CODE
+		AND CMB.NEED_SYNC = 'Y'
+		WHERE NTPL.PROCESS_NAME = 'Posting Enquiry'
+			AND NTPL.STATUS = 'IP_RCT_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Inward Credit Posting'
+			AND NTPL.STATUS = 'IP_BCT_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Inward Credit Posting'
+			AND NTPL.STATUS = 'IP_RCT_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Prepaid Card Ac Posting'
+			AND NTPL.STATUS = 'IP_RCT_PC_T24_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Prepaid Card Pool Ac Posting'
+			AND NTPL.STATUS = 'IP_BCT_PC_T24_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Credit Card Pool Ac Posting'
+			AND NTPL.STATUS = 'IP_BCT_CC_T24_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Credit Card Pool Ac Posting'
+			AND NTPL.STATUS = 'IP_RCT_CC_T24_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Posting Enquiry'
+			AND NTPL.STATUS = 'IP_RCT_CC_T24_POSTING_SUCCESS'
+			OR NTPL.PROCESS_NAME = 'Posting Enquiry'
+			AND NTPL.STATUS = 'IP_BCT_CC_T24_POSTING_SUCCESS' )V
+WHERE PROCESS_TYPE = 'IP' ${cond_params}`
                             }
+
                             if(params.department_code!='' && params.department_code!=null && params.department_code!=undefined)
-                            {
-                               Taketran+=`and DEPARTMENT_NAME='${params.department_code}'`
-                            }
-
-
+                                {
+                                   Taketran+=`and DEPARTMENT_CODE='${params.department_code}'`
+                                }
+       
                             ExecuteQuery1(Taketran, function (arrdata) {
                                 if (arrdata.length) {
 
@@ -380,8 +378,6 @@ process_type = 'OP'  ${cond_params}`
             reqInstanceHelper.SendResponse(serviceName, appResponse, null, objSessionLogInfo, 'IDE_SERVICE_10002', 'ERROR IN ASSIGN LOG INFO FUNCTION', error);
         }
     })
-
-
 
 
 

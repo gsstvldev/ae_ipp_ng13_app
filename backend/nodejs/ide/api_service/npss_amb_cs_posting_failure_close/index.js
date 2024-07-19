88a89086c73d7b034016b3ce695d26169db54d79
administrator <administrator@gsstvl.com>
2024-07-19 05:30:57 +0000

    12556 - for 4962

--- backend/nodejs/ide/api_service/npss_amb_cs_posting_failure_close/index.js
+++ backend/nodejs/ide/api_service/npss_amb_cs_posting_failure_close/index.js
@@ -7,6 +7,241 @@ var app = express.Router();
 
 app.post('/', function(appRequest, appResponse, next) {
 
+    
+    try {
+        /*   Created By :Suresh
+        Created Date :19-07-2024  */
+        var serviceName = 'NPSS AMB (CS) Posting failure close';
+        var reqInstanceHelper = require($REFPATH + 'common/InstanceHelper'); ///  Response,error,info msg printing        
+        var reqTranDBInstance = require($REFPATH + "instance/TranDBInstance.js"); /// postgres & oracle DB pointing        
+        var reqLogInfo = require($REFPATH + 'log/trace/LogInfo'); /// Log information Detail
+        var reqAuditLog = require($REFPATH + 'log/audit/AuditLog');
+        var reqDateFormatter = require($REFPATH + 'common/dateconverter/DateFormatter');
+        var params = appRequest.body.PARAMS; //  Client input fromm Server
+        var headers = appRequest.headers; // header details
+        var objSessionLogInfo = null; // set value is null
+        var mTranConn = "";
+        var success_process_status
+        var success_status
+        var processName
+        var arrTranID
+        var TempTranID
+        var objresponse = {
+            'status': 'FAILURE',
+            'data': '',
+            'msg': ''
+        }; // Response to Client
+        // Assign function for loginformation and session info
+        reqLogInfo.AssignLogInfoDetail(appRequest, function (objLogInfo, objSessionInformation) {
+            try {
+                objSessionLogInfo = objLogInfo; // Assing log information
+                // Log Viewer Setup
+                objSessionLogInfo.HANDLER_CODE = 'NPSS AMB (CS) Posting failure close';
+                objSessionLogInfo.ACTION = 'ACTION';
+                objSessionLogInfo.PROCESS = 'NPSS AMB (CS) Posting failure close';
+                // Get DB Connection                                                                                                                                      
+                reqTranDBInstance.GetTranDBConn(headers, false, function (pSession) {
+                    mTranConn = pSession; //  assign connection    
+                    reqAuditLog.GetProcessToken(pSession, objLogInfo, function prct(error, prct_id) {
+                        try {
+                            var PRCT_ID = prct_id
+                            var take_status = `select success_process_status,success_status from core_nc_workflow_setup where rule_code = '${params.Rule_Code}' and  eligible_status = '${params.eligible_status}' and eligible_process_status = '${params.eligible_process_status}'`
+                            ExecuteQuery1(take_status, function (arrrule) {
+                                if (arrrule.length > 0) {
+                                    success_process_status = arrrule[0].success_process_status;
+                                    success_status = arrrule[0].success_status;
+                                    if (Array.isArray(params.Tran_Id)) {
+                                        arrTranID = params.Tran_Id.map(function (eachTran) {
+                                            return eachTran.toString();
+                                        });
+                                    } else {
+                                        arrTranID = [params.Tran_Id.toString()];
+                                    }
+                                    TempTranID = '(' + "'" + arrTranID.toString().split(',').join("','") + "'" + ')';
+                                    if (params.Roleid == '705' || params.Roleid == 705) {
+                                        processName = 'Manual Close - Maker'
+                                    } else {
+                                        processName = 'Manual Close - Checker'
+                                    }
+                                    var TakeTrnData = `select * from NPSS_CAMT_ACC_CRDR_NTFN   where ncacn_id in ${TempTranID}`
+                                    ExecuteQuery1(TakeTrnData, function (arrprocesslog) {
+                                        if (arrprocesslog.length > 0) {
+                                            var arrCusTranInst = [];
+                                            for (let i = 0; i < arrprocesslog.length; i++) {
+                                                var objCusTranInst = {};
+                                                objCusTranInst.MSG_ID = arrprocesslog[i].header_msg_id ;
+                                                objCusTranInst.PRCT_ID = PRCT_ID;
+                                             
+                                                objCusTranInst.PROCESS_REF_NO = arrprocesslog[i].end_to_end_id ;
+                                                objCusTranInst.PROCESS_TYPE = 'OP'
+                                                objCusTranInst.PROCESS_TIME = reqDateFormatter.GetTenantCurrentDateTime(headers, objSessionLogInfo);
+                                                objCusTranInst.PROCESS_NAME = processName
+                                                objCusTranInst.PROCESSING_SYSTEM = '';
+                                                objCusTranInst.PROCESS_STATUS = success_process_status;
+                                                objCusTranInst.STATUS = success_status;
+                                                objCusTranInst.TENANT_ID = params.TENANT_ID;
+                                                objCusTranInst.APP_ID = '215'
+                                                objCusTranInst.DT_CODE = 'DT_1304_1672471156312'
+                                                objCusTranInst.DTT_CODE = 'DTT_1304_1721132786653'
+                                                objCusTranInst.DT_DESCRIPTION = 'transaction_group'
+                                                objCusTranInst.DTT_DESCRIPTION = 'Transaction'
+                                                objCusTranInst.CREATED_BY = params.CREATED_BY;
+                                                objCusTranInst.CREATED_BY_NAME = params.CREATED_BY_NAME;
+                                                objCusTranInst.CREATED_DATE = reqDateFormatter.GetTenantCurrentDateTime(headers, objSessionLogInfo);
+                                                objCusTranInst.MODIFIED_BY = "";
+                                                objCusTranInst.MODIFIED_BY_NAME = "";
+                                                objCusTranInst.MODIFIED_DATE = null;
+                                                objCusTranInst.SYSTEM_ID = params.SYSTEM_ID;
+                                                objCusTranInst.SYSTEM_NAME = params.SYSTEM_NAME;
+                                                objCusTranInst.CREATED_BY_STS_ID = "";
+                                                objCusTranInst.MODIFIED_BY_STS_ID = "";
+                                                objCusTranInst.created_clientip = objSessionLogInfo.CLIENTIP;
+                                                objCusTranInst.created_tz = objSessionLogInfo.CLIENTTZ;
+                                                objCusTranInst.created_tz_offset = objSessionLogInfo.CLIENTTZ_OFFSET;
+                                                objCusTranInst.created_date_utc = reqDateFormatter.GetCurrentDateInUTC(headers, objSessionLogInfo);
+                                                objCusTranInst.created_by_sessionid = objSessionLogInfo.SESSION_ID;
+                                                objCusTranInst.routingkey = headers.routingkey;
+                                                arrCusTranInst.push(objCusTranInst)
+                                            }
+                                            _BulkInsertProcessItem(arrCusTranInst, 'NPSS_CAMT_PROCESS_LOG', function callbackInsert(CusTranInsertRes) {
+                                                if (CusTranInsertRes.length > 0) {
+                                                  
+                                                
+                                                    var  UpdateTrnTbl = `update NPSS_CAMT_ACC_CRDR_NTFN   set  status='${success_status}',process_status='${success_process_status}',MODIFIED_BY = '${params.CREATED_BY}',MODIFIED_DATE = '${reqDateFormatter.GetTenantCurrentDateTime(headers, objSessionLogInfo)}',MODIFIED_BY_NAME ='${params.CREATED_BY_NAME}',PRCT_ID ='${PRCT_ID}', MODIFIED_CLIENTIP = '${objSessionLogInfo.CLIENTIP}', MODIFIED_TZ = '${objSessionLogInfo.CLIENTTZ}', MODIFIED_TZ_OFFSET = '${objSessionLogInfo.CLIENTTZ_OFFSET}', MODIFIED_BY_SESSIONID = '${objSessionLogInfo.SESSION_ID}', MODIFIED_DATE_UTC = '${reqDateFormatter.GetCurrentDateInUTC(headers, objSessionLogInfo)}'  where ncacn_id in ${TempTranID} `
+                                                   
+                                                    ExecuteQuery(UpdateTrnTbl, function (uptranresult) {
+                                                        if (uptranresult == 'SUCCESS') {
+                                                            objresponse.status = 'Success';
+                                                            sendResponse(null, objresponse)
+                                                        } else {
+                                                            objresponse.status = 'Error in NPSS_CAMT_ACC_CRDR_NTFN Table Update';
+                                                            sendResponse(null, objresponse)
+ 
+                                                        }
+                                                    })
+ 
+                                                } else {
+                                                    objresponse.status = 'Error in NPSS_CAMT_PROCESS_LOG Insert';
+                                                    reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, "IDE_SERVICE_CORE_001", "Insert not succes", result);
+                                                    sendResponse(null, objresponse)
+                                                }
+ 
+                                            })
+ 
+                                        }
+                                        else {
+ 
+                                            objresponse.status = "No Data found in Transaction table"
+                                            sendResponse(null, objresponse)
+                                        }
+ 
+                                    })
+                                }
+                                else {
+ 
+                                    objresponse.status = "No Data found in core nc workflow table"
+                                    sendResponse(null, objresponse)
+                                }
+                            })
+ 
+ 
+                        }
+                        catch (error) {
+                            reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, "IDE_SERVICE_004", "ERROR IN API CALL FUNCTION", error);
+                            sendResponse(error, null);
+                        }
+ 
+                    })
+ 
+ 
+                    function _BulkInsertProcessItem(insertarr, strTrnTableName, callbackInsert) {
+                        try {
+                            reqTranDBInstance.InsertBulkTranDB(mTranConn, strTrnTableName, insertarr, objSessionLogInfo, 300, function callbackInsertBulk(result, error) {
+                                try {
+                                    if (error) {
+                                        reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, 'IDE_SERVICE_10049', 'ERROR IN BULK INSERT FUNCTION', error);
+                                        sendResponse(error)
+                                    } else {
+                                        if (result.length > 0) {
+                                            callbackInsert(result);
+                                        } else {
+                                            callbackInsert([]);
+                                        }
+                                    }
+                                } catch (error) {
+                                    reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, 'IDE_SERVICE_10048', 'ERROR IN BULK INSERT FUNCTION', error);
+                                    sendResponse(error)
+                                }
+                            });
+                        } catch (error) {
+                            reqInstanceHelper.PrintError(serviceName, objSessionLogInfo, 'IDE_SERVICE_10047', 'ERROR IN BULK INSERT FUNCTION', error);
+                            sendResponse(error)
+                        }
+                    }
+                    //Execute Query Function
+                    function ExecuteQuery1(query, callback) {
+                        reqTranDBInstance.ExecuteSQLQuery(mTranConn, query, objSessionLogInfo, function (result, error) {
+                            try {
+                                if (error) {
+                                    sendResponse(error, null);
+                                } else {
+                                    if (result.rows.length > 0) {
+                                        callback(result.rows);
+                                    } else {
+                                        callback([]);
+                                    }
+                                }
+                            } catch (error) {
+                                sendResponse(error, null);
+                            }
+                        });
+                    }
+ 
+                    //Execute Query for common
+                    function ExecuteQuery(query, callback) {
+                        reqTranDBInstance.ExecuteSQLQuery(mTranConn, query, objSessionLogInfo, function (result, error) {
+                            try {
+                                if (error) {
+                                    sendResponse(error)
+                                } else {
+                                    callback("SUCCESS");
+ 
+                                }
+                            } catch (error) {
+                                sendResponse(error)
+                            }
+                        });
+                    }
+ 
+                    //Send Response Function Definition
+                    function sendResponse(error, response) {
+                        try {
+                            if (error) {
+ 
+                                reqInstanceHelper.SendResponse(serviceName, appResponse, null, objSessionLogInfo, 'IDE_SERVICE_10005', '', error);
+ 
+                            } else {
+ 
+                                reqInstanceHelper.SendResponse(serviceName, appResponse, response, objSessionLogInfo)
+ 
+                            }
+                        } catch (error) {
+                            reqInstanceHelper.SendResponse(serviceName, appResponse, null, objSessionLogInfo, 'IDE_SERVICE_10004', 'ERROR IN SEND RESPONSE FUNCTION : ', error);
+                        }
+                    }
+                })
+            } catch (error) {
+                sendResponse(error, null);
+            }
+        })
+    }
+ 
+    catch (error) {
+        sendResponse(error, null);
+    }
+
+
+
 });
 
 module.exports = app;
--- frontend/wp_ng9/custom_widget/npss_amb_cs_posting_failure_close/npss_amb_cs_posting_failure_close.service.ts
+++ frontend/wp_ng9/custom_widget/npss_amb_cs_posting_failure_close/npss_amb_cs_posting_failure_close.service.ts
@@ -1,18 +1,48 @@
 
 import { Injectable } from '@angular/core';
-//Uncomment below lines when it is required
-//import { Http, Response, Headers, RequestOptions, HttpModule } from '@angular/http';
-//import { Observable } from 'rxjs/Observable';
-//import { HttpClient } from '@angular/common/http';
-//import 'rxjs/add/operator/map';
-//import 'rxjs/add/operator/catch';
+import { HttphelperService } from '../../scripts/fx/httphelper.service'; // for server call only use http services
+import { AppHandlerService } from '../../scripts/fx/app.handler.service'; //Show for Dialog for Screen
+import { SCOPE, SESSION } from '../../scripts/fx/session.enum';
+import { SessionService } from '../../scripts/fx/session.service'; // get Session information
+import { DialogService } from '../../scripts/fx/dialog.service'; // Dialog box appear
+import { CoreService } from '../../scripts/fx/core.service'; //core services used for get screen values from memory variables
+
                         
 @Injectable()
 export class npss_amb_cs_posting_failure_closeService {
-    constructor() { }
+   constructor(private httpHelper: HttphelperService,
+        private appHandler: AppHandlerService,
+        private sessionHelper: SessionService,
+        private coreHelper: CoreService,
+        public dialogHelper: DialogService) { }
     //Default calling function
     fn_npss_amb_cs_posting_failure_close(source_id,destn_id,parent_source_id,event_code,event_params,screenInstance,internals,handler_code,event_data,data_source){
-    console.log('NPSS AMB (CS) Posting failure close')
+  var ClientParams: any = {}
+         var CtrlScope = screenInstance['verify_get_deal_ui'].f_npss_verification_get_deal_ui.model;
+      
+        ClientParams.CREATED_BY = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "U_ID");
+        ClientParams.CREATED_BY_NAME = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "LOGIN_NAME");
+        ClientParams.Tran_Id = this.coreHelper.get_value_from_memory("MEMORY_VARIABLES", "MI_LEVEL_NCACN_ID");
+ 
+        ClientParams.eligible_status = this.coreHelper.get_value_from_memory("MEMORY_VARIABLES", "MI_LEVEL_STATUS");
+        ClientParams.eligible_process_status = this.coreHelper.get_value_from_memory("MEMORY_VARIABLES", "MI_LEVEL_PROCESS_STATUS");
+        ClientParams.PROD_CODE = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "PRODUCT_CODE");
+        ClientParams.Roleid = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "APP_USER_ROLES");
+        ClientParams.AppId = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "APP_ID");
+        ClientParams.TENANT_ID = this.sessionHelper.GetVariable(SCOPE.SESSION_LEVEL, "TENANT_ID");
+        ClientParams.RULE_CODE = 'POSTING_FAILURE_CLOSE'
+        ClientParams.screenName = screenInstance.wftpa_description
+        this.CallUrlWithData(ClientParams, screenInstance, internals);
+    }
+       CallUrlWithData(ClientParams, screenInstance, internals) {
+        this.httpHelper.HttpPost('/microsvc/npss_amb_cs_posting_failure_close/', ClientParams)
+            .subscribe((res: any) => {
+                if (res.data.status == "SUCCESS" || res.data == "SUCCESS") 
+                    this.appHandler.callInternals(internals, screenInstance, "SUCCESS");
+               else {
+                    this.dialogHelper.ShowErrorDialog(res.data.status)
+                }
+            });
     }
     //Custom validation logics
     //Uncomment below lines when validation is required

fc9f9c9f7222075523c9adbb6ca68eafe8759ed9
Daseen <daseenh@gsstvl.com>
2024-07-19 12:37:14 +0530

    WI 3962

---
+++ frontend/wp_ng9/custom_widget/npss_amb_cs_posting_failure_close/npss_amb_cs_posting_failure_close.module.ts
@@ -0,0 +1,12 @@
+
+    import { NgModule } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+    import { npss_amb_cs_posting_failure_closeComponent } from './npss_amb_cs_posting_failure_close.component'; 
+    @NgModule({
+        imports: [CommonModule],
+        exports:[npss_amb_cs_posting_failure_closeComponent],
+        declarations: [npss_amb_cs_posting_failure_closeComponent],
+        providers:[]
+    })
+    // Start of class 
+    export class npss_amb_cs_posting_failure_closeModule { }

c31b775b0b226963d2a2e0ad3da979f0244437d4
administrator <administrator@gsstvl.com>
2024-07-30 06:20:05 +0000

    12593 - Report unasign

--- frontend/wp_ng9/meta_data/app_npss_edge/s_report_viewer/s_report_viewer.module.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_report_viewer/s_report_viewer.module.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 35356 
+Build ID        : 36145 
 Modified By     : Admin 
-Modified Date   : 2024-May-09 9:35 AM 
+Modified Date   : 2024-Jul-30 6:18 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_report_viewer

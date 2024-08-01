debbbaed429fd17ab83dba84e969505ec6e0ca27
administrator <administrator@gsstvl.com>
2024-08-01 05:51:59 +0000

    12614 - View camt053 Screen Changes

--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt053/p_view_tran_detail_layout/p_view_tran_detail_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt053/p_view_tran_detail_layout/p_view_tran_detail_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 36171 
+Build ID        : 36173 
 Modified By     : Admin 
-Modified Date   : 2024-Aug-01 4:27 AM 
+Modified Date   : 2024-Aug-01 5:50 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_camt053

5a924946ee35b98c107e547a321875a263aa3561
Daseen <daseenh@gsstvl.com>
2024-08-05 12:57:12 +0530

    3954 camt posting retry

---
+++ frontend/wp_ng9/custom_widget/npss_amb_cs_camt54_posting_failre_retry/npss_amb_cs_camt54_posting_failre_retry.module.ts
@@ -0,0 +1,12 @@
+
+    import { NgModule } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+    import { npss_amb_cs_camt54_posting_failre_retryComponent } from './npss_amb_cs_camt54_posting_failre_retry.component'; 
+    @NgModule({
+        imports: [CommonModule],
+        exports:[npss_amb_cs_camt54_posting_failre_retryComponent],
+        declarations: [npss_amb_cs_camt54_posting_failre_retryComponent],
+        providers:[]
+    })
+    // Start of class 
+    export class npss_amb_cs_camt54_posting_failre_retryModule { }

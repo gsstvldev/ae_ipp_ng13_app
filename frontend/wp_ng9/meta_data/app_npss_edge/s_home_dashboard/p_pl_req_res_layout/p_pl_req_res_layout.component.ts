adcf5b568a12e109a7a70d370b5a350015fd722c
administrator <administrator@gsstvl.com>
2024-08-06 05:18:59 +0000

    12641 - Query Changes in Home Dashboard

--- frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_pl_req_res_layout/p_pl_req_res_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_home_dashboard/p_pl_req_res_layout/p_pl_req_res_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 36108 
+Build ID        : 36212 
 Modified By     : Admin 
-Modified Date   : 2024-Aug-05 13:25 PM 
+Modified Date   : 2024-Aug-06 4:57 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_home_dashboard
--- frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt053/p_main_layout/p_main_layout.component.ts
+++ frontend/wp_ng9/meta_data/app_npss_edge/s_view_camt053/p_main_layout/p_main_layout.component.ts
@@ -1,9 +1,9 @@
 /* ---------------------------------------------------------------------------
 UI Framework    : Angular
 Version         : 5.0 
-Build ID        : 36210 
+Build ID        : 36211 
 Modified By     : Admin 
-Modified Date   : 2024-Aug-06 4:42 AM 
+Modified Date   : 2024-Aug-06 4:49 AM 
 Generated From  : TORUS Low Code Platform 
 Copyright       : Torus Innovations Pvt Ltd © Copyright 2018 
 Screen Name     : s_view_camt053
